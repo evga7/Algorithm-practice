8200ba4ed9c32b7541e07cb1b72ff2606a7c77cc
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2023-03-23 12:33:42 +0900

    [unrated] Title: 귤 고르기, Time: 20.49 ms, Memory: 18.1 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.py
@@ -1,10 +1,8 @@
 from collections import *
 def solution(k, tangerine):
     answer = 0
-    m=defaultdict(int)
-    for cur in tangerine:
-        m[cur]+=1
-    for cur in sorted(list(m.values()),key=lambda x:-x):
+    a=Counter(tangerine).values()
+    for cur in sorted(a,reverse=True):
         k-=cur
         answer+=1
         if k<=0:
