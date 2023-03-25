4bc7073e36ede74da26a6c07e7576458a162b1b9
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2023-03-25 21:36:35 +0900

    [level 3] Title: 단속카메라, Time: 0.89 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv3/42884. 단속카메라/단속카메라.py
+++ 프로그래머스/lv3/42884. 단속카메라/단속카메라.py
@@ -1,10 +1,11 @@
 def solution(routes):
     answer = 0
-    q=[]
     routes.sort(key=lambda x:x[1])
+    b=-30001
     for cur in routes:
         s=cur[0]
         e=cur[1]
-        if q and q[-1]>=s:continue
-        q.append(e)
-    return len(q)
+        if b>=s:continue
+        answer+=1
+        b=e
+    return answer
