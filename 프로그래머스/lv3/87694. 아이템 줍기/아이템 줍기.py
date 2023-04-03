859091c15e26eae09fdb5bac2804d917d097f48b
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2023-04-04 02:11:26 +0900

    [level 3] Title: 아이템 줍기, Time: 1.09 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/87694. 아이템 줍기/아이템 줍기.py
+++ 프로그래머스/lv3/87694. 아이템 줍기/아이템 줍기.py
@@ -7,28 +7,16 @@ def solution(rectangle, characterX, characterY, itemX, itemY):
     itemX*=2
     characterY*=2
     characterX*=2
-    a=[[0 for _ in range(104)] for _ in range(104)]
-    b = [[0 for _ in range(104)] for _ in range(104)]
+    a=[[-1 for _ in range(104)] for _ in range(104)]
     v = [[0 for _ in range(104)] for _ in range(104)]
     for cur in rectangle:
         x1,y1,x2,y2=cur[0]*2,cur[1]*2,cur[2]*2,cur[3]*2
         for i in range(x1,x2+1):
             for j in range(y1,y2+1):
-                a[i][j]=1
+                if x1<i<x2 and y1<j<y2:a[i][j]=0
+                elif a[i][j]!=0:
+                    a[i][j]=1
     q=deque()
-    q.append((0,0))
-    v[0][0]=1
-    while q:
-        x,y=q.popleft()
-        for i in range(8):
-            n_x=x+dx[i]
-            n_y=y+dy[i]
-            if 0<=n_x<=101 and 0<=n_y<=101 and not v[n_x][n_y]:
-                v[n_x][n_y] = 1
-                if a[n_x][n_y]:
-                    b[n_x][n_y]=1
-                    continue
-                q.append((n_x,n_y))
     v = [[987654321 for _ in range(104)] for _ in range(104)]
     q.append((characterX,characterY,0))
     v[characterX][characterY]=0
@@ -39,6 +27,6 @@ def solution(rectangle, characterX, characterY, itemX, itemY):
         for i in range(4):
             n_x=x+dx[i]
             n_y=y+dy[i]
-            if 1<=n_x<=100 and 1<=n_y<=100 and v[n_x][n_y]>c+1 and b[n_x][n_y]:
+            if 1<=n_x<=101 and 1<=n_y<=101 and v[n_x][n_y]>c+1 and a[n_x][n_y]==1:
                 v[n_x][n_y]=c+1
                 q.append((n_x,n_y,c+1))
