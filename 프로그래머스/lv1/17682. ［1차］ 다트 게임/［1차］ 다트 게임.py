12ca571e93e5dda85265d4a42b263f70a0d5a063
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2023-04-05 22:39:30 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.14 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
@@ -1,51 +1,19 @@
-def c(s):
-    if s=='D':
-        return 2
-    elif s=='T':
-        return 3
-    return 1
+import re
 def solution(dartResult):
-    answer = 0
-    a=[]
-    s=""
-    for i,cur in enumerate(dartResult):
-        s += cur
-        if cur=='D' or cur=='S' or cur=='T':
-            if i+1<len(dartResult):
-                if dartResult[i + 1] == '*' or dartResult[i + 1] == '#':continue
-            a.append(s)
-            s=""
-        elif cur=='*' or cur=='#':
-            a.append(s)
-            s=""
-    if s:
-        a.append(s)
-    b=0
     aa=[]
-    idx=0
-    for cur in a:
-        num=""
-        for cc in cur:
-            if not str.isdigit(cc):break
-            num+=cc
-        if len(num)>1:
-            bonus=cur[2]
-        else:
-            bonus=cur[1]
-        num=int(num)
-        if cur[-1]=='#' or cur[-1]=='*':
-            op=cur[2]
-            num=pow(num,c(bonus))
-            if op=='*':
-                op=2
-                if idx-1>=0:
-                    aa[idx-1]*=2
-            else:
-                op=-1
-            num*=op
-            aa.append(num)
-        else:
-            aa.append(pow(num, c(bonus)))
-        idx+=1
-
+    a=re.compile('(\d+)([SDT])([#*]?)')
+    v=a.findall(dartResult)
+    bo={'S':1,'D':2,'T':3}
+    for cur in v:
+        num=int(cur[0])
+        bonus=cur[1]
+        op=cur[2]
+        c=num**bo[bonus]
+        if op=='*':
+            if aa:
+                aa[-1]*=2
+            c*=2
+        elif op=='#':
+            c*=-1
+        aa.append(c)
     return sum(aa)
