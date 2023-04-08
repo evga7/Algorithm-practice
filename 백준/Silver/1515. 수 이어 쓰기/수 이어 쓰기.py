61321c2fda650e25eec26915cf9d46945f4fad31
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2023-04-09 04:26:56 +0900

    [Silver III] Title: 수 이어 쓰기, Time: 160 ms, Memory: 116376 KB -BaekjoonHub

--- 백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.py
+++ 백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.py
@@ -1,17 +1,16 @@
+from collections import *
 import sys
 def input():return sys.stdin.readline().rstrip()
-N=input()
-s=""
+N=deque(input())
 i=0
 while True:
     i+=1
-    s+=str(i)
+    s=deque(str(i))
     idx=0
-    idx2=0
-    while idx<len(N) and idx2<len(s):
-        if N[idx]==s[idx2]:
-            idx+=1
-        idx2+=1
-    if idx==len(N):
+    while N and idx<len(s):
+        if N[0]==s[idx]:
+            N.popleft()
+        idx+=1
+    if not N:
         break
 print(i)
