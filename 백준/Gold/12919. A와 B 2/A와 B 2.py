91196e91bcc11926cee7072f5ed136ea0d811018
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2023-04-17 20:59:30 +0900

    [Gold V] Title: A와 B 2, Time: 128 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Gold/12919. A와 B 2/A와 B 2.py
+++ 백준/Gold/12919. A와 B 2/A와 B 2.py
@@ -1,27 +1,16 @@
-import heapq
 import sys
 def input():return sys.stdin.readline().rstrip()
 s=input()
 s2=input()
 st=set()
-q=[]
-heapq.heappush(q,(0,s2))
-res=0
-while q:
-    cost,cur=heapq.heappop(q)
-    if cur==s:
-        res=1
-        break
-    if cur[-1]=='A':
-        nxt=cur[:len(cur)-1]
-        if not nxt in st and len(nxt)>=len(s):
-            st.add(nxt)
-            heapq.heappush(q,(cost+1,nxt))
-    if cur[0]=='B':
-        nxt=cur[1:]
-        nxt=nxt[::-1]
-        if not nxt in st and len(nxt)>=len(s):
-            st.add(nxt)
-            heapq.heappush(q,(cost+1,nxt))
+def go(s,s2):
+    if s==s2:
+        print(1)
+        exit(0)
+    if s2[-1]=='A' and len(s2)-1>=len(s):
+        go(s,s2[:len(s2)-1])
+    if s2[0]=='B' and len(s2)-1>=len(s):
+        go(s,s2[1:][::-1])
+res=go(s,s2)
 
-print(res)
+print(0)
