ec7024812460cdab0cc79b9d9ed746cad1e2237e
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2023-04-18 20:51:49 +0900

    [Gold V] Title: 탑, Time: 376 ms, Memory: 212620 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.py
+++ 백준/Gold/2493. 탑/탑.py
@@ -3,11 +3,13 @@ def input():return sys.stdin.readline().rstrip()
 N=int(input())
 arr=list(map(int,input().split()))
 st=[]
+res_v=[]
 for i,cur in enumerate(arr):
     while st and arr[st[-1]]<cur:
         st.pop()
     if st:
-        print(st[-1]+1,end=' ')
+        res_v.append(st[-1]+1)
     else:
-        print(0,end=' ')
+        res_v.append(0)
     st.append(i)
+print(*res_v)
