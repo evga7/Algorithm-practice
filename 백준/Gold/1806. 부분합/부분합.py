84fbf014e5a8b2a6ce4e60b995d9b9c05cdf30e1
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2023-04-23 09:05:04 +0900

    [Gold IV] Title: 부분합, Time: 144 ms, Memory: 125300 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.py
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -4,16 +4,17 @@ N,M=map(int,input().split())
 a=list(map(int,input().split()))
 res=int(1e20)
 left,right=0,0
-s=0
-while left<N:
-    if s<M and right<N:
-        s+=a[right]
-        right+=1
+s=a[0]
+while True:
+    if s>=M:
+        res = min(res, right - left+1)
+        s -= a[left]
+        left += 1
     else:
-        if s>=M:
-            res=min(res,right-left)
-        s-=a[left]
-        left+=1
+        right += 1
+        if right==N:
+            break
+        s += a[right]
 
 if res==int(1e20):
     res=0
