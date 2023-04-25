2cf7b712a8fbdb4c2767741cd7351899c84eaf49
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2023-04-25 10:39:06 +0900

    [Gold III] Title: 탑 보기, Time: 244 ms, Memory: 133184 KB -BaekjoonHub

--- 백준/Gold/22866. 탑 보기/탑 보기.py
+++ 백준/Gold/22866. 탑 보기/탑 보기.py
@@ -1,63 +1,34 @@
-# N,M=map(int,input().split())
-# visited=[[987654321]*M for _ in range(N)]
-# arr=[list(map(int,input().strip())) for _ in range(N)]
-# str=re.findall(r'\d+',str) 숫자만 때기
-# re.findall(r'-?\d+', str1)] 음수는 이렇게
-# word1 = re.findall("[a-zA-Z]+", st) 이건 문자 추출
-#arr2 = [k[::-1] for k in zip(*arr)] 문자열 90도 회전
-import bisect
-import math
-import re
-import heapq
 import sys
-from collections import *
-import itertools
-import collections
-si=sys.stdin.readline
-#sys.setrecursionlimit(10 ** 6)
-dx = [0,-1,0,1]  # 왼 위 오 아래
-dy = [-1,0,1,0]
 def input():return sys.stdin.readline().rstrip()
 N=int(input())
-arr=list(map(int,input().split()))
+a=list(map(int,input().split()))
 st=[]
-res=[0 for _ in range(N+1)]
-res2=[100001 for _ in range(N+1)]
-for i,cur in enumerate(arr):
-    if not st:
-        st.append(i)
-    else:
-        while st and cur>=arr[st[-1]]:
-            st.pop()
-        res[i]+=len(st)
-        if st:
-            if res2[i]==100001:
-                res2[i]=st[-1]
-        st.append(i)
-
+res=[0 for _ in range(N)]
+res2=[0 for _ in range(N)]
+for i in range(N):
+    cur=a[i]
+    while st and a[st[-1]]<=cur:
+        st.pop()
+    res[i]+=len(st)
+    if st:
+        res2[i]=st[-1]+1
+    st.append(i)
 st=[]
 for i in range(N-1,-1,-1):
-    cur=arr[i]
-    if not st:
-        st.append(i)
-    else:
-        while st and cur>=arr[st[-1]]:
-            st.pop()
-        res[i]+=len(st)
-        if st:
-            if res2[i]==100001:
-                res2[i]=st[-1]
-            else:
-                dist=abs(st[-1]-i)
-                dist2=abs(res2[i]-i)
-                if dist==dist2:
-                    res2[i]=min(res2[i],st[-1])
-                else:
-                    if dist<dist2:
-                        res2[i]=st[-1]
-        st.append(i)
+    cur=a[i]
+    while st and a[st[-1]]<=cur:
+        st.pop()
+    res[i] += len(st)
+    if st:
+        if res2[i]:
+            if abs(res2[i]-1-i)>abs(st[-1]-i):
+                res2[i]=st[-1]+1
+        else:
+            res2[i]=st[-1]+1
+    st.append(i)
 for i in range(N):
-    if res2[i]!=100001:
-        print(res[i],res2[i]+1)
+    if res[i]:
+        print(res[i],end=' ')
+        print(res2[i])
     else:
-        print(res[i])
+        print(0)
