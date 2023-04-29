a855d2a0e42c1904e36e30401d85c3038e8114da
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2023-04-29 17:21:11 +0900

    [Gold III] Title: ACM Craft, Time: 600 ms, Memory: 135364 KB -BaekjoonHub

--- 백준/Gold/1005. ACM Craft/ACM Craft.py
+++ 백준/Gold/1005. ACM Craft/ACM Craft.py
@@ -7,24 +7,21 @@ def go():
     for i in range(1,N+1):
         if not de[i]:
             q.append(i)
-            dist[i]=a[i]
+            dist[i]=a[i-1]
     while q:
         x=q.popleft()
         for nxt in g[x]:
             de[nxt]-=1
             if not de[nxt]:
                  q.append(nxt)
-            dist[nxt]=max(dist[nxt],dist[x]+a[nxt])
+            dist[nxt]=max(dist[nxt],dist[x]+a[nxt-1])
     return dist[W]
 while T:
     T-=1
     N,K=map(int,input().split())
     de = [0 for _ in range(N + 1)]
     g=[[] for _ in range(N+1)]
-    a=[0]
-    b=list(map(int,input().split()))
-    for cur in b:
-        a.append(cur)
+    a=list(map(int,input().split()))
     dist=[0 for _ in range(N+1)]
     for i in range(K):
         c,d=map(int,input().split())
