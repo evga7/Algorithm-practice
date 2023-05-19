3411519efc11bcddc853e4c80e720a06ea5df520
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2023-05-19 20:14:39 +0900

    [Gold V] Title: 배, Time: 5296 ms, Memory: 115748 KB -BaekjoonHub

--- 백준/Gold/1092. 배/배.py
+++ 백준/Gold/1092. 배/배.py
@@ -6,21 +6,20 @@ a=list(map(int,input().split()))
 M=int(input())
 b=list(map(int,input().split()))
 b.sort(reverse=True)
-q=[-cur for cur in a]
-heapq.heapify(q)
-if -q[0]<b[0]:
+a.sort()
+if a[N-1]<b[0]:
     print(-1)
     exit(0)
 res=0
 cnt=0
 chk=[0 for _ in range(M)]
 while cnt<M:
-    temp=q[:]
+    temp=a[:]
     while temp:
         idx=0
-        while temp and idx+1<M and (-temp[0]<b[idx] or chk[idx]):
+        while temp and idx+1<M and (temp[0]<b[idx] or chk[idx]):
             idx+=1
-        if not chk[idx] and -temp[0]>=b[idx]:
+        if not chk[idx] and temp[0]>=b[idx]:
             chk[idx] = 1
             cnt += 1
         heapq.heappop(temp)
