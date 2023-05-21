67b464df33f0a33a975e5a320b0e43ef5639924b
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2023-05-21 18:41:14 +0900

    [Gold V] Title: 호텔, Time: 420 ms, Memory: 118360 KB -BaekjoonHub

--- 백준/Gold/1106. 호텔/호텔.py
+++ 백준/Gold/1106. 호텔/호텔.py
@@ -2,22 +2,21 @@ import sys
 def input():return sys.stdin.readline().rstrip()
 C,N=map(int,input().split())
 a=[list(map(int,input().split())) for _ in range(N)]
-dp=[[[-1 for _ in range(1001)] for _ in range(20)] for _ in range(1001)]
-def go(people,idx,cnt):
+dp=[[-1 for _ in range(20)] for _ in range(1001)]
+def go(people,idx):
     if people >= C:
         return 0
     if idx==N:
         if people>=C:
             return 0
         return 987654321
-    if dp[people][idx][cnt]!=-1:
-        return dp[people][idx][cnt]
+    if dp[people][idx]!=-1:
+        return dp[people][idx]
     ret=987654321
     cur_cost=a[idx][0]
     cur_p=a[idx][1]
-    c=C//cur_p
     for i in range(0,1001):
-        ret=min(ret,go(people+i*cur_p,idx+1,c)+i*cur_cost)
-    dp[people][idx][cnt]=ret
-    return dp[people][idx][cnt]
-print(go(0,0,0))
+        ret=min(ret,go(people+i*cur_p,idx+1)+i*cur_cost)
+    dp[people][idx]=ret
+    return dp[people][idx]
+print(go(0,0))
