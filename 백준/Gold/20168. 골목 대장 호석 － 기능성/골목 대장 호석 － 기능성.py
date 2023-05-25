0ad2e4fe90f809fdde69546cf6e4ed5128365fd1
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2023-05-26 03:51:07 +0900

    [Gold V] Title: 골목 대장 호석 - 기능성, Time: 140 ms, Memory: 114436 KB -BaekjoonHub

---
+++ 백준/Gold/20168. 골목 대장 호석 － 기능성/골목 대장 호석 － 기능성.py
@@ -0,0 +1,25 @@
+import sys
+from collections import *
+def input():return sys.stdin.readline().rstrip()
+N,M,S,E,C=map(int,input().split())
+g=[[] for _ in range(N+1)]
+for i in range(M):
+    a,b,cost=map(int,input().split())
+    g[a].append((b,cost))
+    g[b].append((b, cost))
+q=deque()
+dist=[987654321 for _ in range(N+1)]
+q.append((S,0,0))
+while q:
+    x,cost,money=q.popleft()
+    if x==E:
+        print(cost)
+        exit(0)
+    for cur in g[x]:
+        nxt=cur[0]
+        n_money=money+cur[1]
+        n_cost=max(cost,cur[1])
+        if n_money<=C and dist[nxt]>n_cost:
+            dist[nxt]=n_cost
+            q.append((nxt,n_cost,n_money))
+print(-1)
