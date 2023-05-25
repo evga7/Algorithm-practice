a7b10f465f14129928121de6c6ac0bdd008e1a74
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2023-05-26 01:58:39 +0900

    [Gold IV] Title: 같은 수로 만들기, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Gold/2374. 같은 수로 만들기/같은 수로 만들기.py
@@ -0,0 +1,16 @@
+import sys
+def input():return sys.stdin.readline().rstrip()
+N=int(input())
+a=[int(input()) for _ in range(N)]
+st=[]
+res=0
+mn=max(a)
+for cur in a:
+    if st and st[-1]==cur:continue
+    if st and st[-1]<cur:
+        res+=cur-st[-1]
+    if st:
+        st.pop()
+    st.append(cur)
+res+=mn*len(st)-sum(st)
+print(res)
