c06cfb6253945c6aa624d8d1e4b45d7e8bf5aad6
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2023-05-25 21:26:17 +0900

    [Gold IV] Title: 점프, Time: 364 ms, Memory: 174992 KB -BaekjoonHub

--- 백준/Gold/2253. 점프/점프.py
+++ 백준/Gold/2253. 점프/점프.py
@@ -1,3 +1,7 @@
+from collections import *
+import heapq
+dx=[0,0,1,-1]
+dy=[1,-1,0,0]
 import sys
 def input():return sys.stdin.readline().rstrip()
 N,M=map(int,input().split())
@@ -5,19 +9,19 @@ st=set()
 for i in range(M):
     num=int(input())
     st.add(num)
-dp=[[-1 for _ in range(501)] for _ in range(10001)]
-def go(idx,speed):
-    if idx==N:
-        return 0
-    if dp[idx][speed]!=-1:
-        return dp[idx][speed]
-    ret=987654321
-    for cur in (speed+1,speed,speed-1):
-        if cur>0 and idx+cur<=N and not idx+cur in st:
-            ret=min(ret,go(idx+cur,cur)+1)
-    dp[idx][speed]=ret
-    return dp[idx][speed]
-res=go(1,0)
-if res==987654321:
-    res=-1
-print(res)
+q=deque()
+dist=[[987654321 for _ in range(501)] for _ in range(10001)]
+q.append((0,1,0))
+while q:
+    cost,pos,speed=q.popleft()
+    if pos==N:
+        print(cost)
+        exit(0)
+    i=0
+    for cur in (speed-1,speed,speed+1):
+        nxt=pos+cur
+        if cur>0 and nxt<=N and not nxt in st and dist[pos][cur]>cost+1:
+            dist[pos][cur]=cost+1
+            q.append((cost+1,nxt,cur))
+        i+=1
+print(-1)
