3b0438a16bf2c96c3e1cff9e7a8c61e4f4a7809b
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2023-06-05 13:38:58 +0900

    [Gold V] Title: 조 짜기, Time: 136 ms, Memory: 115340 KB -BaekjoonHub

--- 백준/Gold/2229. 조 짜기/조 짜기.py
+++ 백준/Gold/2229. 조 짜기/조 짜기.py
@@ -2,16 +2,16 @@ import sys
 def input():return sys.stdin.readline().rstrip()
 N=int(input())
 a=list(map(int,input().split()))
-dp=[[-1 for _ in range(N+2)] for _ in range(2)]
-def go(op,idx):
+dp=[-1 for _ in range(N+2)]
+def go(idx):
     if idx>=N:
         return 0
-    if dp[op][idx]!=-1:
-        return dp[op][idx]
+    if dp[idx]!=-1:
+        return dp[idx]
     ret=0
-    ret=max(ret,go(0,idx+1))
+    ret=max(ret,go(idx+1))
     for i in range(idx,N):
-        ret=max(ret,go(1,i+1)+abs(a[idx]-a[i]))
-    dp[op][idx]=ret
-    return dp[op][idx]
-print(go(0,0))
+        ret=max(ret,go(i+1)+abs(a[idx]-a[i]))
+    dp[idx]=ret
+    return dp[idx]
+print(go(0))
