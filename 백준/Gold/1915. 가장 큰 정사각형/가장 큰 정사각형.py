abeded5bbd67ebf17a81c8ff0867ec26dae3a3f7
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2023-06-10 18:41:29 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 728 ms, Memory: 225884 KB -BaekjoonHub

--- 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.py
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.py
@@ -1,5 +1,5 @@
 import sys
-sys.setrecursionlimit(100000)
+sys.setrecursionlimit(10000)
 def input(): return sys.stdin.readline().rstrip()
 N,M=map(int,input().split())
 a=[list(map(int,input())) for _ in range(N)]
