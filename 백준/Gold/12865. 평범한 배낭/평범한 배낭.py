1611b7fa6eed4e5ff523a030cd6e00f2e2259836
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2023-06-22 14:05:40 +0900

    [Gold V] Title: 평범한 배낭, Time: 1100 ms, Memory: 210560 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,37 +1,17 @@
-# N,M=map(int,input().split())
-# visited=[[987654321]*M for _ in range(N)]
-# arr=[list(map(int,input().strip())) for _ in range(N)]
-# str=re.findall(r'\d+',str) 숫자만 때기
-# re.findall(r'-?\d+', str1)] 음수는 이렇게
-# word1 = re.findall("[a-zA-Z]+", st) 이건 문자 추출
-import re
-import heapq
 import sys
-from collections import *
-import itertools
-import collections
-
-sys.setrecursionlimit(10 ** 5)
-dx = [0, 1, 0, -1]  # 오 아래 왼 위
-dy = [1, 0, -1, 0]
-dp=[[-1 for _ in range(100001)]for _ in range(101)]
-wv=[]
-N,M=map(int,sys.stdin.readline().rstrip().split())
-for i in range(N):
-    wv.append(list(map(int,sys.stdin.readline().rstrip().split())))
-
-def go(cnt,wei):
-    if cnt>=N:
+def input(): return sys.stdin.readline().rstrip()
+N,M=map(int,input().split())
+a=[list(map(int,input().split())) for _ in range(N)]
+dp=[[-1 for _ in range(100001)] for _ in range(101)]
+def go(idx,cnt):
+    if idx==N:
         return 0
-    if dp[cnt][wei]!=-1:
-        return dp[cnt][wei]
-    dp[cnt][wei]=go(cnt+1,wei)
-    if wei-wv[cnt][0]>=0:
-        dp[cnt][wei]=max(dp[cnt][wei],go(cnt+1,wei-wv[cnt][0])+wv[cnt][1])
-    return dp[cnt][wei]
-
-print(go(0,M))
-
-
-
-
+    if dp[idx][cnt]!=-1:
+        return dp[idx][cnt]
+    ret=0
+    if cnt+a[idx][0]<=M:
+        ret=max(ret,go(idx+1,cnt+a[idx][0])+a[idx][1])
+    ret=max(ret,go(idx+1,cnt))
+    dp[idx][cnt]=ret
+    return dp[idx][cnt]
+print(go(0,0))
