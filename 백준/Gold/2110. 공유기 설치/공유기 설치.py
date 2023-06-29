24604802582c5134dea6d6f193c54e4e585c39f6
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2023-06-30 01:11:42 +0900

    [Gold IV] Title: 공유기 설치, Time: 212 ms, Memory: 116560 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.py
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -1,23 +1,20 @@
 import sys
-import math
-from collections import *
-def input():return sys.stdin.readline().rstrip()
+def input(): return sys.stdin.readline().rstrip()
 N,M=map(int,input().split())
 a=[int(input()) for _ in range(N)]
+left=0
+right=int(1e15)
 a.sort()
-left=1
-right=int(1e10)
 while left<=right:
     mid=left+right>>1
     cnt=1
     back=a[0]
     for i in range(1,N):
-        cur=a[i]
-        if cur-back<mid:continue
-        back=cur
-        cnt+=1
+        if a[i]-back>mid:
+            cnt+=1
+            back=a[i]
     if cnt>=M:
         left=mid+1
     else:
         right=mid-1
-print(left-1)
+print(left)
