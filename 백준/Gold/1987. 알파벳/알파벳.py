408c717701f351e642b10d48cc151b5bbf3d6d44
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2023-07-03 21:35:39 +0900

    [Gold IV] Title: 알파벳, Time: 756 ms, Memory: 278100 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,38 +1,29 @@
-# N,M=map(int,input().split())
-# visited=[[987654321]*M for _ in range(N)]
-# arr=[list(map(int,input().strip())) for _ in range(N)]
-# str=re.findall(r'\d+',str) 숫자만 때기
-# re.findall(r'-?\d+', str1)] 음수는 이렇게
-# word1 = re.findall("[a-zA-Z]+", st) 이건 문자 추출
-import re
-import heapq
-import sys
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
 from collections import *
-import itertools
-sys.setrecursionlimit(10 ** 5)
-#input=sys.stdin.readline()
-dx = [-1, 1, 0, 0]  # 위 아래 왼 오
-dy = [0, 0, -1, 1]
+def is_inside(x, y, N, M):
+    return 0 <= x < N and 0 <= y < M
+import sys
+def input(): return sys.stdin.readline().rstrip()
+def cov(s):
+    return ord(s)-ord('A')
 N,M=map(int,input().split())
-visited=[[[[0]*21 for _ in range(21)] for _ in range(21) ]for _ in range(21)]
-arr=[]
-for i in range(N):
-    arr.append(list(input()))
-for i in range(N):
-    for j in range(M):
-        arr[i][j]=1<<(ord(arr[i][j])-ord('A'))
-
+a=[list(input()) for _ in range(N)]
+st=set()
+q=deque()
 res=0
-def go(x,y,cost,bit):
-    global res
+q.append((0,0,1,1<<(cov(a[0][0]))))
+while q:
+    x,y,cost,bit=q.popleft()
     res=max(res,cost)
     for i in range(4):
         n_x=x+dx[i]
         n_y=y+dy[i]
-        if 0<=n_x<N and 0<=n_y<M:
-            c= arr[n_x][n_y]
-            if not bit & c:
-                go(n_x,n_y,cost+1,bit|c)
-go(0,0,1,arr[0][0])
-
+        if is_inside(n_x,n_y,N,M):
+            chk_bit=cov(a[n_x][n_y])
+            if not (bit & (1<<chk_bit)):
+                n_bit=bit|(1<<chk_bit)
+                if not (n_x,n_y,n_bit) in st:
+                    q.append((n_x,n_y,cost+1,n_bit))
+                    st.add((n_x,n_y,n_bit))
 print(res)
