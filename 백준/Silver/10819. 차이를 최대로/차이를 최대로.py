885767f101b4f4b6ae01c09c04350db73d282b02
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2023-07-29 02:10:47 +0900

    [Silver II] Title: 차이를 최대로, Time: 128 ms, Memory: 114976 KB -BaekjoonHub

--- 백준/Silver/10819. 차이를 최대로/차이를 최대로.py
+++ 백준/Silver/10819. 차이를 최대로/차이를 최대로.py
@@ -1,24 +1,13 @@
+from itertools import *
 import sys
 def input(): return sys.stdin.readline().rstrip()
 N=int(input())
 a=list(map(int,input().split()))
-v=[]
-visited=[0 for _ in range(8)]
 res=0
-def go(cnt):
-    if cnt==N:
-        global res
-        s=0
-        for i in range(N-1):
-            s+=abs(v[i]-v[i+1])
-        res=max(res,s)
-        return
-    for i in range(N):
-        if visited[i]:continue
-        visited[i]=1
-        v.append(a[i])
-        go(cnt+1)
-        visited[i]=0
-        v.pop()
-go(0)
+aa=permutations(a)
+for cur in aa:
+    s=0
+    for i in range(N-1):
+        s+=abs(cur[i]-cur[i+1])
+    res=max(res,s)
 print(res)
