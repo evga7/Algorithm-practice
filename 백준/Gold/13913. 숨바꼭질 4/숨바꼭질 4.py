0b0b5b05a837c9b27f0f599508abbbc54ae486c4
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2023-08-09 05:41:08 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 280 ms, Memory: 133656 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.py
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.py
@@ -1,40 +1,28 @@
-# N,M=map(int,input().split())
-# visited=[[987654321]*M for _ in range(N)]
-# arr=[list(map(int,input().strip())) for _ in range(N)]
-# str=re.findall(r'\d+',str) 숫자만 때기
-# re.findall(r'-?\d+', str1)] 음수는 이렇게
-# word1 = re.findall("[a-zA-Z]+", st) 이건 문자 추출
-import re
+import heapq
+def is_inside(x,y,N,M):
+    return 0<=x<N and 0<=y<M
 import sys
-from collections import *
-import itertools
-sys.setrecursionlimit(10 ** 6)
-dx = [-1, 1, 0, 0]  # 위 아래 왼 오
-dy = [0, 0, -1, 1]
+def input():return sys.stdin.readline().rstrip()
+INF=sys.maxsize
+st=[0 for _ in range(100001)]
+visited=set()
 N,M=map(int,input().split())
-visited=[987654321]*100001
-q=deque()
-q.append((N,0))
-m={}
-visited[N]=0
+q=[]
+q.append((0,N))
 while q:
-    cur,cost=q.popleft()
-    if cur==M:
+    cost,x=heapq.heappop(q)
+    if x==M:
+        print(cost)
         break
-    for next in (cur*2,cur+1,cur-1):
-        if 0<=next<=100000 and cost+1<visited[next]:
-            visited[next]=cost+1
-            m[next]=cur
-            q.append((next,cost+1))
-s=[]
-MM=M
-s.append(M)
-while MM!=N :
-    s.append(m[MM])
-    MM=m[MM]
-s.reverse()
-print(visited[M])
-for cur in s:
-    print(cur,end=' ')
-
-
+    for nxt in (x*2,x-1,x+1):
+        if 0<=nxt<=100000 and not nxt in visited:
+            visited.add(nxt)
+            heapq.heappush(q,(cost+1,nxt))
+            st[nxt]=x
+res_v=[]
+while M!=N:
+    res_v.append(M)
+    M=st[M]
+res_v.append(N)
+res_v.reverse()
+print(*res_v)
