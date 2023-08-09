cd366b86ab0e6bf2d5a1549d6958f178fa6d2070
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2023-08-09 21:02:36 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 140 ms, Memory: 114328 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
@@ -0,0 +1,9 @@
+import sys
+def input():return sys.stdin.readline().rstrip()
+INF=sys.maxsize
+a,b,c,d,e,f=map(int,input().split())
+for i in range(-999,1000):
+    for j in range(-999,1000):
+        if a*i+b*j==c and d*i+e*j==f:
+            print(i,j)
+            exit(0)
