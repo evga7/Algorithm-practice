6b4643728b72cc1aa4561e0113e321c08eae15c1
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2023-09-08 19:38:57 +0900

    [Silver III] Title: 1로 만들기, Time: 148 ms, Memory: 122952 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,12 +1,17 @@
 import sys
 def input():return sys.stdin.readline().rstrip()
-dp=[987654321 for _ in range(10000001)]
+INF=sys.maxsize
+
+def is_inside(x, y, N, M):
+    return 0 <= x < N and 0 <= y < M
 N=int(input())
+res=0
+dp=[0 for _ in range(N+1)]
 dp[1]=0
 for i in range(2,N+1):
-    if not i%3:
+    dp[i]=dp[i-1]+1
+    if i%3==0 and i//3>0:
         dp[i]=min(dp[i],dp[i//3]+1)
-    if not i%2:
+    if i%2==0 and i//2>0:
         dp[i]=min(dp[i],dp[i//2]+1)
-    dp[i]=min(dp[i],dp[i-1]+1)
 print(dp[N])
