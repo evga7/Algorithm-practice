b726f0ee66dcddb140cb957df4b75982316ccbc2
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2023-09-18 21:52:27 +0900

    [level 2] Title: 상품 별 오프라인 매출 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/131533. 상품 별 오프라인 매출 구하기/상품 별 오프라인 매출 구하기.sql
@@ -0,0 +1,4 @@
+-- 코드를 입력하세요
+SELECT a.PRODUCT_CODE ,sum(b.SALES_AMOUNT*a.PRICE) SALES from PRODUCT a  left join OFFLINE_SALE b on a.PRODUCT_ID=b.PRODUCT_ID
+group by a.PRODUCT_CODE
+order by SALES desc,a.PRODUCT_CODE
