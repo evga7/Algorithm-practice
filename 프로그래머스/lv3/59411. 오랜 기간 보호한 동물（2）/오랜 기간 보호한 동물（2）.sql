40170fd9d89a08c8988e546b59d326971ba3883e
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2023-09-20 19:26:57 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
+++ 프로그래머스/lv3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -1,3 +1,3 @@
 -- 코드를 입력하세요
-SELECT a.ANIMAL_ID,a.name from ANIMAL_INS a join ANIMAL_OUTS b on a.ANIMAL_ID=b.ANIMAL_ID
-order by datediff(b.datetime,a.datetime) desc limit 2
+SELECT a.ANIMAL_ID,a.NAME from ANIMAL_INS a join ANIMAL_OUTS b on a.ANIMAL_ID=b.ANIMAL_ID
+order by datediff(b.DATETIME,a.DATETIME) desc limit 2
