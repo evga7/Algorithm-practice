bac84ae8bebbf05241b8731a9ac71a7870426d5e
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2023-09-20 18:55:31 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/lv3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,4 +1,4 @@
 -- 코드를 입력하세요
-SELECT a.NAME,a.DATETIME from ANIMAL_INS a left join ANIMAL_OUTS b on a.ANIMAL_ID=b.ANIMAL_ID 
+SELECT a.NAME,a.DATETIME DATETIME from ANIMAL_INS a left join ANIMAL_OUTS b on a.ANIMAL_ID=b.ANIMAL_ID
 where b.ANIMAL_ID is null
 order by DATETIME limit 3
