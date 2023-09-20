e8a9641cf7a5a7907f7fd788f017eef5d3b3c7ee
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2023-09-21 03:57:24 +0900

    [unrated] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
+++ 프로그래머스/unrated/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -1,5 +1,6 @@
+
 -- 코드를 입력하세요
-SELECT concat('/home/grep/src/',b.BOARD_ID,+'/',b.FILE_ID,b.FILE_NAME,b.FILE_EXT) FILE_PATH
+SELECT concat('/home/grep/src',+'/',b.BOARD_ID,+'/',b.FILE_ID,b.FILE_NAME,b.FILE_EXT) FILE_PATH 
 from USED_GOODS_BOARD a join USED_GOODS_FILE b on a.BOARD_ID=b.BOARD_ID
-where views in (select max(views) from USED_GOODS_BOARD)
-order by b.FILE_id desc
+where VIEWS in (select max(views) from USED_GOODS_BOARD)
+order by b.FILE_ID desc
