ce2d561e932969c2eab9c593fd3951d2031bfe66
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2023-09-21 03:00:23 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
+++ 프로그래머스/lv3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -1,5 +1,4 @@
 -- 코드를 입력하세요
-select id ,name ,host_id from places
-where HOST_ID in (select host_id from places group by host_id having count(*)>1)
+SELECT ID,NAME,HOST_ID from PLACES
+where HOST_ID in (select HOST_ID from places group by host_id having count(host_id)>=2)
 order by id
-    
