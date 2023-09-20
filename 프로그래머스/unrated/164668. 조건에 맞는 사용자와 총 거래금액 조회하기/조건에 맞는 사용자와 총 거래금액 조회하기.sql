9758a903609669b5e2738f6e449d164991bd379b
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2023-09-20 19:45:44 +0900

    [unrated] Title: 조건에 맞는 사용자와 총 거래금액 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/164668. 조건에 맞는 사용자와 총 거래금액 조회하기/조건에 맞는 사용자와 총 거래금액 조회하기.sql
+++ 프로그래머스/unrated/164668. 조건에 맞는 사용자와 총 거래금액 조회하기/조건에 맞는 사용자와 총 거래금액 조회하기.sql
@@ -1,6 +1,6 @@
 -- 코드를 입력하세요
-SELECT b.USER_ID,b.NICKNAME,sum(price) TOTAL_SALES from USED_GOODS_BOARD a join USED_GOODS_USER b on a.WRITER_ID=b.USER_ID
+SELECT b.USER_ID,b.NICKNAME ,sum(a.PRICE) TOTAL_SALES from USED_GOODS_BOARD a join USED_GOODS_USER b on a.WRITER_ID=b.USER_ID
 where a.STATUS='DONE'
-group by b.user_id
+group by b.user_id,b.nickname
 having TOTAL_SALES>=700000
 order by TOTAL_SALES
