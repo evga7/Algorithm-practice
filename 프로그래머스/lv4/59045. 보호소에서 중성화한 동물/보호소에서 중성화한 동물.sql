b194df065d8928e6b472e4aa271c844b3fbb29f2
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2023-09-22 02:53:02 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/lv4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -1,5 +1,5 @@
 -- 코드를 입력하세요
 SELECT a.ANIMAL_ID,a.ANIMAL_TYPE,a.NAME from ANIMAL_INS a join ANIMAL_OUTS b on a.ANIMAL_ID=b.ANIMAL_ID
-where a.SEX_UPON_INTAKE like '%Intact%' and 
-(b.SEX_UPON_OUTCOME like '%Spayed%' or b.SEX_UPON_OUTCOME like '%Neutered%')
-order by a.animal_id
+where a.SEX_UPON_INTAKE like '%Intact%' and (b.SEX_UPON_OUTCOME like '%Spayed%' or
+                                            b.SEX_UPON_OUTCOME like '%Neutered%')
+order by animal_id
