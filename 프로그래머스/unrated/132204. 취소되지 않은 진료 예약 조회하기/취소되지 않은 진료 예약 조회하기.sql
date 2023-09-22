cc229b82fa00154f22a54d2feace1c52b7c269f2
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2023-09-22 23:19:56 +0900

    [unrated] Title: 취소되지 않은 진료 예약 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
+++ 프로그래머스/unrated/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -1,5 +1,5 @@
 -- 코드를 입력하세요
-SELECT c.APNT_NO,a.PT_NAME,a.PT_NO,b.MCDP_CD,b.DR_NAME,c.APNT_YMD from PATIENT a join APPOINTMENT c on a.PT_NO=c.PT_NO 
-join DOCTOR b on b.DR_ID=c.MDDR_ID
-where c.APNT_CNCL_YN ='N' and APNT_YMD like '%2022-04-13%' and c.MCDP_CD='CS'
+SELECT c.APNT_NO,a.PT_NAME,a.PT_NO,c.MCDP_CD,b.DR_NAME,c.APNT_YMD from DOCTOR b join APPOINTMENT  c on b.DR_ID=c.MDDR_ID
+join PATIENT a on a.PT_NO=c.PT_NO
+where c.MCDP_CD ='CS' and c.APNT_YMD like '%2022-04-13%' and c.APNT_CNCL_YMD is null
 order by c.APNT_YMD
