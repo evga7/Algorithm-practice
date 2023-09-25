d389d99d02337f3a60b9e16aa3463a49765e6d5d
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2023-09-26 02:47:48 +0900

    [Gold V] Title: 가장 긴 짝수 연속한 부분 수열 (large), Time: 412 ms, Memory: 224708 KB -BaekjoonHub

--- 백준/Gold/22862. 가장 긴 짝수 연속한 부분 수열 （large）/가장 긴 짝수 연속한 부분 수열 （large）.py
+++ 백준/Gold/22862. 가장 긴 짝수 연속한 부분 수열 （large）/가장 긴 짝수 연속한 부분 수열 （large）.py
@@ -1,9 +1,5 @@
 import sys
 def input():return sys.stdin.readline().rstrip()
-INF=sys.maxsize
-
-def is_inside(x, y, N, M):
-    return 0 <= x < N and 0 <= y < M
 N,M=map(int,input().split())
 a=list(map(int,input().split()))
 cnt=0
@@ -13,11 +9,10 @@ res=0
 while right<N:
     if a[right]&1:
         cnt+=1
-    while cnt>M:
+    while left<right and cnt>M:
         if a[left]&1:
             cnt-=1
         left+=1
     right+=1
-    res=max(right-left-cnt,res)
-
+    res=max(res,right-left-cnt)
 print(res)
