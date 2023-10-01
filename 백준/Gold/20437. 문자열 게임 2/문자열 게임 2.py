da6754c525759942d0faf31437a3ef54d21883c2
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2023-10-01 18:32:43 +0900

    [Gold V] Title: 문자열 게임 2, Time: 3708 ms, Memory: 134788 KB -BaekjoonHub

--- 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.py
+++ 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.py
@@ -8,16 +8,19 @@ while T:
     n=int(input())
     res1=987654321
     res2=0
+    if n==1:
+        res1,res2=1,1
     left=0
     right=0
     m2=defaultdict(list)
     while right<len(s):
         m2[s[right]].append(right)
         if len(m2[s[right]])>=n:
-            cur=right
-            before=m2[s[right]][-n]
-            res1=min(res1,cur-before+1)
-            res2=max(res2,cur-before+1)
+            for i in range(n-1,len(m2[s[right]])):
+                cur=m2[s[right]][i]
+                before=m2[s[right]][i-(n-1)]
+                res1=min(res1,cur-before+1)
+                res2=max(res2,cur-before+1)
         right+=1
     if not res2 or res1==987654321:
         print(-1)
