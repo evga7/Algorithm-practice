e9c1865cb9bc69f8b1a2fbf83c18d2a4fba01f59
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2023-10-08 21:31:39 +0900

    [Silver III] Title: 두 수의 합, Time: 240 ms, Memory: 145184 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.py
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.py
@@ -1,15 +1,34 @@
-N=int(input())
-a=list(map(int,input().split()))
-X=int(input())
-a.sort()
-left,right=0,N-1
+# N,M=map(int,input().split())
+# visited=[[987654321]*M for _ in range(N)]
+# arr=[list(map(int,input().strip())) for _ in range(N)]
+# str=re.findall(r'\d+',str) 숫자만 때기
+# re.findall(r'-?\d+', str1)] 음수는 이렇게
+# word1 = re.findall("[a-zA-Z]+", st) 이건 문자 추출
+#arr2 = [k[::-1] for k in zip(*arr)] 문자열 90도 회전
+import math
+import re
+import heapq
+import sys
+from collections import *
+import itertools
+import collections
+si=sys.stdin.readline
+#sys.setrecursionlimit(10 ** 6)
+dx = [0,-1,0,1]  # 왼 위 오 아래
+dy = [-1,0,1,0]
+def input():return sys.stdin.readline().rstrip()
 res=0
-while left<right:
-    s=a[left]+a[right]
-    if s>=X:
-        if s==X:
-            res+=1
-        right-=1
-    else:
-        left+=1
-print(res)
+N=int(si().rstrip())
+arr=list(map(int,input().split()))
+M=int(input())
+left=0
+right=0
+s=0
+m=defaultdict(int)
+for cur in arr:
+    m[cur]+=1
+
+for cur in m:
+    if M-cur in m:
+        res+=1
+print(res//2)
