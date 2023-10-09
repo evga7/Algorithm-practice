fa9019c3bc3e46d429a83c76d5970e0c191f4669
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2023-10-10 07:19:11 +0900

    [Gold III] Title: 5, Time: 116 ms, Memory: 114328 KB -BaekjoonHub

---
+++ 백준/Gold/3165. 5/5.py
@@ -0,0 +1,18 @@
+import sys
+#sys.setrecursionlimit(100000)
+def input():return sys.stdin.readline().rstrip()
+INF=sys.maxsize
+N,M=map(int,input().split())
+S=list(str(N+1))
+def go(s,pos):
+    while True:
+        if s.count('5')>=M:
+            print(''.join(s))
+            exit(0)
+        while s[pos]=='5' and pos-1>=0:
+            pos-=1
+        num=int(''.join(s))
+        num=num+pow(10,len(s)-pos-1)
+        s=list(str(num))
+        pos=len(s)-1
+go(S,len(S)-1)        
