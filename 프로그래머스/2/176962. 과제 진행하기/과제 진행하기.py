5acab9a715f385d04911de9ecdef19a4d9b15d82
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2023-10-14 08:59:01 +0900

    [level 2] Title: 과제 진행하기, Time: 79.17 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.py
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.py
@@ -4,7 +4,7 @@ def solution(plans):
     v = []
     for s, t, p in plans:
         a, b = map(int, t.split(':'))
-        v.append((a * 60 + b, s, int(p)))
+        v.append([a * 60 + b, s, int(p)])
     v.sort()
     dq=deque()
     idx=0
@@ -13,22 +13,14 @@ def solution(plans):
         t+=1
         if dq and dq[0][0]+dq[0][2]<=t:
             answer.append(dq.popleft()[1])
-            dq2=deque()
-            while dq:
-                a,b,c=dq.popleft()
-                dq2.append((t,b,c))
-            for cur in dq2:
-                dq.append(cur)
+            for i in range(len(dq)):
+                dq[i][0]=t    
         if idx<len(plans) and v[idx][0]<=t:
-            dq2=deque()
             if dq:
-                a,b,c=dq.popleft()
-                dq2.appendleft((t,b,c-(t-a)))
-            while dq:
-                a,b,c=dq.popleft()
-                dq2.append((t,b,c))
-            dq2.appendleft(v[idx])
-            for cur in dq2:
-                dq.append(cur)
+                dq[0][2]-=(t-dq[0][0])
+                dq[0][0]=t
+            for i in range(1,len(dq)):
+                dq[i][0]=t
+            dq.appendleft(v[idx])
             idx+=1
     return answer
