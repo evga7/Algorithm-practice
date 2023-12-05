f267dd4cd7ef7017a5a8608eb9a26029ccb1677a
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2023-12-05 10:59:46 +0900

    [Bronze I] Title: 피카츄, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/14405. 피카츄/피카츄.py
+++ 백준/Bronze/14405. 피카츄/피카츄.py
@@ -1,10 +1,12 @@
 import sys
-def input(): return sys.stdin.readline().rstrip()
+#sys.setrecursionlimit(100000)
+def input():return sys.stdin.readline().rstrip()
+INF=sys.maxsize
 s=input()
-s=s.replace('pi',' ')
-s=s.replace('ka',' ')
-s=s.replace('chu',' ')
-s=s.replace(' ','')
+s=s.replace("pi"," ")
+s=s.replace("ka"," ")
+s=s.replace("chu"," ")
+s=s.replace(" ","")
 if s:
     print("NO")
 else:
