d116c33496d0de84d9e286960890e1f40441cd9b
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-01-23 18:21:35 +0900

    [Gold IV] Title: 민준이와 마산 그리고 건우, Time: 196 ms, Memory: 113020 KB -BaekjoonHub

--- 백준/Gold/18223. 민준이와 마산 그리고 건우/민준이와 마산 그리고 건우.py
+++ 백준/Gold/18223. 민준이와 마산 그리고 건우/민준이와 마산 그리고 건우.py
@@ -1,32 +1,36 @@
-from collections import *
-def is_inside(x, y, N, M):
-    return 0 <= x < N and 0 <= y < M
+import heapq
+def is_inside(x,y,N,M):
+    return 0<=x<N and 0<=y<M
+
 import sys
-def input(): return sys.stdin.readline().rstrip()
-V,E,P=map(int,input().split())
-g=[[] for _ in range(V+1)]
-for i in range(E):
+#sys.setrecursionlimit(100000)
+def input():return sys.stdin.readline().rstrip()
+#map(int,input().split())
+INF=sys.maxsize
+N,M,P=map(int,input().split())
+g=[[] for _ in range(N+1)]
+pos=[0 for _ in range(N+1)]
+
+for i in range(M):
     a,b,c=map(int,input().split())
     g[a].append((b,c))
     g[b].append((a,c))
-res1=987654321
-res2=0
-def go(start,end):
-    dist=[987654321 for _ in range(V+1)]
-    q=deque()
-    q.append((start,0))
-    dist[start]=0
+def go(S,E,op):
+    q=[]
+    q.append((0,S))
+    dist = [987654321 for _ in range(N + 1)]
     while q:
-        x,cost=q.popleft()
-        for cur in g[x]:
-            nxt,n_cost=cur[0],cur[1]+cost
+        cost,cur=heapq.heappop(q)
+        if cur==E:
+            return cost
+        for x in g[cur]:
+            nxt=x[0]
+            n_cost=cost+x[1]
             if dist[nxt]>n_cost:
                 dist[nxt]=n_cost
-                q.append((nxt,n_cost))
-    return dist[end]
-res1=go(1,P)+go(P,V)
-res2=go(1,V)
-if res1<=res2:
+                heapq.heappush(q,(n_cost,nxt))
+    
+if go(1,N,1)==go(1,P,0)+go(P,N,0):
     print("SAVE HIM")
 else:
     print("GOOD BYE")
