7d372e1a3ced025b343dd3921c5e3e2cbb728bd4
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-01-31 15:53:19 +0900

    [Gold V] Title: 평범한 배낭, Time: 932 ms, Memory: 332880 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,17 +1,22 @@
 import sys
+sys.setrecursionlimit(100000)
 def input(): return sys.stdin.readline().rstrip()
+# map(int,input().split())
+INF = sys.maxsize
 N,M=map(int,input().split())
 a=[list(map(int,input().split())) for _ in range(N)]
-dp=[[-1 for _ in range(100001)] for _ in range(101)]
-def go(idx,cnt):
+dp=[[-1 for _ in range(M+1)] for _ in range(N+1)]
+def go(idx,w):
     if idx==N:
         return 0
-    if dp[idx][cnt]!=-1:
-        return dp[idx][cnt]
+    if dp[idx][w]!=-1:
+        return dp[idx][w]
     ret=0
-    if cnt+a[idx][0]<=M:
-        ret=max(ret,go(idx+1,cnt+a[idx][0])+a[idx][1])
-    ret=max(ret,go(idx+1,cnt))
-    dp[idx][cnt]=ret
-    return dp[idx][cnt]
+    b,c=a[idx][0],a[idx][1]
+    if w+b<=M:
+        ret=max(ret,go(idx+1,w+b)+c)
+    ret=max(ret,go(idx+1,w))
+    dp[idx][w]=ret
+    return dp[idx][w]
 print(go(0,0))
+
