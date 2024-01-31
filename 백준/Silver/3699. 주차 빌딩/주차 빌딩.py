c292147cbed474af277f25f748f343531e7ac7a6
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-01-31 15:02:06 +0900

    [Silver V] Title: 주차 빌딩, Time: 212 ms, Memory: 114132 KB -BaekjoonHub

--- 백준/Silver/3699. 주차 빌딩/주차 빌딩.py
+++ 백준/Silver/3699. 주차 빌딩/주차 빌딩.py
@@ -1,42 +1,41 @@
-import sys
+
 from collections import *
-from functools import cmp_to_key
-dx = [0,-1,0,1]  # 오 위 왼 아
-dy = [1,0,-1,0]
-def is_inside(x,y,N,M):
-    return 0<=x<N and 0<=y<M
-def input():return sys.stdin.readline().rstrip()
-INF=sys.maxsize
+import sys
+# sys.setrecursionlimit(100000)
+def input(): return sys.stdin.readline().rstrip()
+# map(int,input().split())
+INF = sys.maxsize
 T=int(input())
-def go(idx):
+res=0
+def f(idx):
     for i in range(N):
         for j in range(M):
             if a[i][j]==idx:
-                return i,j
+                return (i,j)
+    return (-1,-1)
 while T:
     T-=1
     N,M=map(int,input().split())
     a=[deque(map(int,input().split())) for _ in range(N)]
-    mx=0
-    for cur in a:
-        mx=max(mx,max(cur))
-    res=0
     idx=1
-    while idx<=mx:
-        n_x,n_y=go(idx)
+    x, y = 0, 0
+    while True:
+        dx, dy = f(idx)
+        if dx==-1:
+            break
+        #오른쪽으로 움직이기
         cnt=0
-        cur_q = a[n_x]
-        if n_y<M-n_y:
-            while cur_q[0]!=idx:
-                cur_q.append(cur_q.popleft())
+        if M-dy<=dy:
+            while a[dx][0]!=idx:
                 cnt+=1
+                a[dx].appendleft(a[dx].pop())
         else:
-            while cur_q[0]!=idx:
-                cur_q.appendleft(cur_q.pop())
+            while a[dx][0]!=idx:
                 cnt+=1
-        p=n_x*20
-        p+=cnt*5
-        res+=p
+                a[dx].append(a[dx].popleft())
+        a[dx][0]=-1
+        res+=dx*20
+        res+=cnt*5
         idx+=1
-
     print(res)
+    res=0
