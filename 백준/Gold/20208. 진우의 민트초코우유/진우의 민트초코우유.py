7d286fc0c6b35c847b300df9dfa40bae307d031b
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-02-07 22:50:45 +0900

    [Gold V] Title: 진우의 민트초코우유, Time: 1440 ms, Memory: 119320 KB -BaekjoonHub

--- 백준/Gold/20208. 진우의 민트초코우유/진우의 민트초코우유.py
+++ 백준/Gold/20208. 진우의 민트초코우유/진우의 민트초코우유.py
@@ -1,41 +1,35 @@
-def is_inside(x,y,N,M):
-    return 0<x<=N and 0<y<=M
-import heapq
-import math
-from collections import *
 import sys
-def input():return sys.stdin.readline().rstrip()
+# sys.setrecursionlimit(100000)
+def input(): return sys.stdin.readline().rstrip()
+#
+INF = sys.maxsize
 N,M,H=map(int,input().split())
+dist=[[[-1 for _ in range(N+1)] for _ in range(N+1)] for _ in range(1025)]
 a=[list(map(int,input().split())) for _ in range(N)]
-v=[0 for _ in range(11)]
-q=[]
-sx,sy=0,0
+sx,sy=-1,-1
+v=[]
+visited=[0 for _ in range(11)]
 res=0
 for i in range(N):
     for j in range(N):
         if a[i][j]==1:
             sx,sy=i,j
         elif a[i][j]==2:
-            q.append((i,j))
-def cal(sx,sy,dx,dy):
-    return abs(sx-dx)+abs(sy-dy)
-def go(cnt,x,y,hp):
-    global res
-    if cal(sx,sy,x,y)<=hp:
+            v.append((i,j))
+def dis(x,y,n_x,n_y):
+    return abs(x-n_x)+abs(y-n_y)
+def go(x,y,hp,cnt):
+    if hp<0:return
+    if hp-dis(x,y,sx,sy)>=0 and cnt:
+        global res
         res=max(res,cnt)
-    for i in range(len(q)):
-        xx,yy=q[i][0],q[i][1]
-        if v[i]:continue
-        n_hp=hp-cal(x,y,xx,yy)
-        if n_hp>=0:
-            v[i]=1
-            go(cnt+1,xx,yy,n_hp+H)
-            v[i]=0
-for i,cur in enumerate(q):
-    x,y=cur[0],cur[1]
-    hp=M-cal(sx,sy,x,y)
-    if hp>=0:
-        v[i]=1
-        go(1,x,y,hp+H)
-        v[i]=0
+    for i in range(len(v)):
+        if visited[i]:continue
+        n_x, n_y = v[i][0], v[i][1]
+        d=dis(x, y, n_x, n_y)
+        if hp-d>=0:
+            visited[i]=1
+            go(n_x,n_y,hp-d+H,cnt+1)
+            visited[i]=0
+go(sx,sy,M,0)
 print(res)
