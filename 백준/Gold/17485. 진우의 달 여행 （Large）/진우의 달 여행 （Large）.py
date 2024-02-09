f1f50387153bf8b7116d67a183556073c37995d4
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-02-09 23:46:31 +0900

    [Gold V] Title: 진우의 달 여행 (Large), Time: 1908 ms, Memory: 255544 KB -BaekjoonHub

--- 백준/Gold/17485. 진우의 달 여행 （Large）/진우의 달 여행 （Large）.py
+++ 백준/Gold/17485. 진우의 달 여행 （Large）/진우의 달 여행 （Large）.py
@@ -1,9 +1,16 @@
 import sys
-def input():return sys.stdin.readline().rstrip()
-dy=[-1,0,1]
+# sys.setrecursionlimit(100000)
+def input(): return sys.stdin.readline().rstrip()
+#
+INF = sys.maxsize
+dx = [0,-1,0,1]  # 오 위 왼 아
+dy = [1,0,-1,0]
+import heapq
+def is_inside(x,y,N,M):
+    return 0<=x<N and 0<=y<M
 N,M=map(int,input().split())
 a=[list(map(int,input().split())) for _ in range(N)]
-dp=[[[-1 for _ in range(4)] for _ in range(M)]for _ in range(N)]
+dp=[[[-1 for _ in range(4)] for _ in range(M+1)] for _ in range(N+1)]
 def go(x,y,op):
     if x==N:
         return 0
@@ -11,13 +18,13 @@ def go(x,y,op):
         return dp[x][y][op]
     ret=987654321
     for i in range(3):
-        if op==i:continue
+        n_x=x+1
         n_y=y+dy[i]
-        if 0<=n_y<M:
-            ret=min(ret,go(x+1,n_y,i)+a[x][y])
+        if op==i:continue
+        if is_inside(n_x,n_y,N+1,M):
+            ret=min(ret,go(n_x,n_y,i)+a[x][y])
     dp[x][y][op]=ret
     return dp[x][y][op]
-
 res=987654321
 for i in range(M):
     res=min(res,go(0,i,3))
