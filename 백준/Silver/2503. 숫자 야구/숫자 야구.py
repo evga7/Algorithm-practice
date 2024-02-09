f39c7f7f6b0e8b0cde180e75fd97e547812bdbce
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-02-09 22:45:08 +0900

    [Silver III] Title: 숫자 야구, Time: 160 ms, Memory: 111656 KB -BaekjoonHub

--- 백준/Silver/2503. 숫자 야구/숫자 야구.py
+++ 백준/Silver/2503. 숫자 야구/숫자 야구.py
@@ -1,26 +1,34 @@
 import sys
-def input():return sys.stdin.readline().rstrip()
+# sys.setrecursionlimit(100000)
+def input(): return sys.stdin.readline().rstrip()
+#
+INF = sys.maxsize
 N=int(input())
-st=set()
+a=[list(map(int,input().split())) for _ in range(N)]
+def ck(s1,s2):
+    s_cnt=0
+    b_cnt=0
+    st1=set(s1)
+    st2 = set(s2)
+    for i in range(3):
+        if s1[i]==s2[i]:
+            s_cnt+=1
+            st1.remove(s1[i])
+            st2.remove(s1[i])
+    for cur in st1:
+        if cur in st2:
+            b_cnt+=1
+    return (s_cnt,b_cnt)
+stt=set()
 for i in range(123,988):
-    s=str(i)
-    if s.count(s[0])>1 or s.count(s[1])>1 or s.count(s[2])>1 or s.count('0')>0:continue
-    st.add(i)
-for i in range(N):
-    num,s,b=map(int,input().split())
-    num=str(num)
-    for j in range(123,988):
-        s_cnt=0
-        b_cnt=0
-        num2=str(j)
-        for k in range(3):
-            if num2[k] in num:
-                if num2.index(num2[k])==num.index(num2[k]):
-                    s_cnt+=1
-                else:
-                    b_cnt+=1
-        if s_cnt==s and b_cnt==b:
-            continue
-        if j in st:
-            st.remove(j)
-print(len(st))
+    ss = str(i)
+    if ss[0] == ss[1] or ss[1] == ss[2] or ss[0]==ss[2] or ss.find('0')>=0: continue
+    stt.add(i)
+for cur in a:
+    st,S,B=str(cur[0]),cur[1],cur[2]
+    for i in range(123,988):
+        ss=str(i)
+        if ss[0]==ss[1] or ss[1]==ss[2] or ss[0]==ss[2] or ss.find('0')>=0:continue
+        if i in stt and not ck(ss,st)==(S,B) :
+            stt.remove(i)
+print(len(stt))
