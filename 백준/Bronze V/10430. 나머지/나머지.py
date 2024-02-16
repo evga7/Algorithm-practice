1957d10ad50a32cb30a6935fae27e910d0052456
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-02-16 16:21:05 +0900

    [Bronze V] Title: 나머지, Time: 128 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze V/10430. 나머지/나머지.py
@@ -0,0 +1,9 @@
+import sys
+INF = sys.maxsize
+def input():
+    return sys.stdin.readline().rstrip()
+A,B,C=map(int,input().split())
+print((A+B)%C)
+print(((A % C) + (B % C)) % C)
+print( (A * B) % C)
+print(((A % C) * (B % C)) % C)
