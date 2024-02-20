22d4e41aab81f8c8e97eb25809cb650208a5b3b4
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-02-20 20:36:53 +0900

    [Silver II] Title: 제곱수의 합, Time: 36 ms, Memory: 2420 KB -BaekjoonHub

--- 백준/Silver/1699. 제곱수의 합/제곱수의 합.cc
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.cc
@@ -1,26 +1,46 @@
-#include <iostream>
-#include <math.h>
-#include <queue>
-#include <string.h>
-#include <cmath>
-#include <string>
+#include <bits/stdc++.h>
+#define MAX 987654321
+#define MIN -987654321
+#define MOD 1000000007
 using namespace std;
-int N;
+typedef long long int ll;
+typedef pair<int, int> pi;
+typedef pair<int, string> pi3;
+typedef pair<int, pair<int, int>>pi2;
+typedef vector<vector<int>> vpi;
+typedef vector<int> vec;
+typedef vector<pi> vec2;
+typedef queue<pi> que;
+typedef vector<vector<pi>> gra;
+int dx[4] = { 0,1,0,-1 };
+int dy[4] = { 1,0,-1,0 };
+int N, M;
+int res;
+int T;
 int dp[100001];
-
+int solve(int idx)
+{
+	if (idx == 0)return 0;
+	int& ret = dp[idx];
+	if (ret != -1)return ret;
+	ret = MAX;
+	int i;
+	for (i = sqrt(idx); i >=1; i--)
+	{
+		ret = min({ ret,solve(idx - (i * i)) + 1});
+	}
+	return ret;
+}
 int main()
 {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
 	int i, j;
-	scanf("%d", &N);
-	for (i = 1; i <= N; i++)
-	{
-		for (j = 1; j <=sqrt(i); j++)
-		{
-			if (dp[i] > dp[i - j * j] + 1 || dp[i] == 0)
-			{
-				dp[i] = dp[i - j * j] + 1;
-			}
-		}
-	}
-	printf("%d", dp[N]);
+	cin >> N;
+	memset(dp, -1, sizeof(dp));
+	cout<<solve(N);
 }
+
+
+
