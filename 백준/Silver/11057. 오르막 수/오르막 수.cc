b14717efecede9021f235560c0634bd64c7313c3
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-02-20 23:55:42 +0900

    [Silver I] Title: 오르막 수, Time: 0 ms, Memory: 2056 KB -BaekjoonHub

--- 백준/Silver/11057. 오르막 수/오르막 수.cc
+++ 백준/Silver/11057. 오르막 수/오르막 수.cc
@@ -1,27 +1,53 @@
-#include <stdio.h>
-
-long long dp[1001][1001];
+#include <bits/stdc++.h>
+#define MAX 987654321
+#define MIN -987654321
+#define MOD 1000000009
+#define pb push_back
+#define pob pop_back
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pi;
+typedef pair<int, string> pi3;
+typedef pair<int, pair<int, int>>pi2;
+typedef vector<vector<int>> vpi;
+typedef vector<int> vec;
+typedef vector<pi> vec2;
+typedef queue<pi> que;
+typedef vector<vector<pi>> gra;
+int dx[4] = { 0,1,0,-1 };
+int dy[4] = { 1,0,-1,0 };
+int N, M;
+int res;
+int num, num2;
+int T;
+int S;
+vec v;
+vec v2;
+int dp[1001][10];
+int solve(int idx, int cnt)
+{
+	if (idx == 0)return 1;
+	int& ret = dp[idx][cnt];
+	if (ret != -1)return ret;
+	ret = 0;
+	int i;
+	for (i = cnt; i <= 9; i++)
+	{
+		ret += solve(idx - 1, i) % 10007;
+	}
+	return ret % 10007;
+}
 int main()
 {
-	int i, j,k;
-	for (i = 0; i < 10; i++)
-		dp[1][i] = 1;
-	int N;
-	long long sum = 0;
-	scanf("%d", &N);
-	for (i = 2; i <= N; i++)
-	{
-		for (j = 0; j < 10; j++)
-		{
-			for (k = j; k < 10; k++)
-			{
-				dp[i][k] += dp[i-1][j];
-				dp[i][k] %= 10007;
-			}
-		}
-	}
-	for (i = 0; i < 10; i++)
-		sum += dp[N][i];
-	printf("%lld", sum%10007);
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int i, j;
+	memset(dp, -1, sizeof(dp));
+	cin >> N;
+	cout << solve(N, 0);
 }
+
+
+
 
