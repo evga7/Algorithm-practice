e0dc99bc13df597ef70d85e78bc0caed5971070b
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-02-20 18:09:40 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.cc
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.cc
@@ -1,43 +1,43 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <queue>
-#include <string>
-#include <sstream>
-#include <stack>
-#include <set>
-#include <map>
-#define MAX 987654321
-#define MIN -987654321
+#include <bits/stdc++.h>
+#define MAX 2147483647
+#define MIN -2147483648
+#define MOD 1000000000
 using namespace std;
 typedef long long int ll;
 typedef pair<int, int> pi;
-int N;
+typedef pair<int, string> pi3;
+typedef pair<int, pair<int, int>>pi2;
+typedef vector<vector<int>> vpi;
+typedef vector<int> vec;
+typedef vector<pi> vec2;
+typedef queue<pi> que;
+int dx[4] = { 0,1,0,-1 };
+int dy[4] = { 1,0,-1,0 };
+int N, M;
+int num;
+ll res;
 ll dp[101][10];
-int arr[10001];
+ll solve(int idx, int cnt)
+{
+	if (idx == N) return 1;
+	ll& ret = dp[idx][cnt];
+	if (ret!=-1) return ret;
+	if (cnt == 0)
+		return ret = solve(idx + 1, cnt + 1) % MOD;
+	else if (cnt == 9)
+		return ret = solve(idx + 1, cnt - 1) % MOD;
+	else
+		return ret = (solve(idx + 1, cnt - 1) + solve(idx+1, cnt + 1)) % MOD;
+}
 int main()
 {
-	ios_base::sync_with_stdio(false); cin.tie(NULL);
-	int i;
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int i, j;
 	cin >> N;
+	memset(dp, -1, sizeof(dp));
 	for (i = 1; i <= 9; i++)
-		dp[1][i] = 1;
-	for (i = 2; i <=N; i++)
-	{
-		dp[i][0] = dp[i - 1][1];
-		dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]);
-		dp[i][2] = (dp[i - 1][1] + dp[i - 1][3]) % 1000000000;
-		dp[i][3] = (dp[i - 1][2] + dp[i - 1][4]) % 1000000000;
-		dp[i][4] = (dp[i - 1][3] + dp[i - 1][5]) % 1000000000;
-		dp[i][5] = (dp[i - 1][4] + dp[i - 1][6]) % 1000000000;
-		dp[i][6] = (dp[i - 1][5] + dp[i - 1][7]) % 1000000000;
-		dp[i][7] = (dp[i - 1][6] + dp[i - 1][8]) % 1000000000;
-		dp[i][8] = (dp[i - 1][7] + dp[i - 1][9]) % 1000000000;
-		dp[i][9] = (dp[i - 1][8]);
-
-	}
-	ll res = 0;
-	for (i = 0; i < 10; i++)
-		res += dp[N][i]% 1000000000;
-	cout << res % 1000000000;
+		res += solve(1, i) % MOD;
+	cout << res%MOD;
 }
