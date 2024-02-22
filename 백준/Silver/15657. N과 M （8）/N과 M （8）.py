aeb056052f79464768da183d7527b5ad310605e0
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-02-22 19:21:33 +0900

    [Silver III] Title: N과 M (8), Time: 180 ms, Memory: 113244 KB -BaekjoonHub

--- 백준/Silver/15657. N과 M （8）/N과 M （8）.py
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.py
@@ -1,17 +1,18 @@
 import sys
-def input():return sys.stdin.readline().rstrip()
-def is_inside(x,y,N,M):
-    return 0<=x<N and 0<=y<M
+def input():return sys.stdin.readline().rstrip() 
 N,M=map(int,input().split())
+v=[]
 a=list(map(int,input().split()))
-v=[0 for _ in range(M)]
+visited=[0 for _ in range(N+1)]
 a.sort()
 def go(idx,cnt):
     if cnt==M:
         print(*v)
         return
     for i in range(idx,N):
-        v[cnt]=a[i]
+        if visited[i]:continue
+        v.append(a[i])
         go(i,cnt+1)
-        v[cnt]=0
+        v.pop()
 go(0,0)
+    
