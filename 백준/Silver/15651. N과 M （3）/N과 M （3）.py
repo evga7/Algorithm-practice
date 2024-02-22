0e049256972925074227013432675bab35a2ff3b
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-02-22 18:24:47 +0900

    [Silver III] Title: N과 M (3), Time: 716 ms, Memory: 152428 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,14 @@
+import sys
+def input():return sys.stdin.readline().rstrip() 
+N,M=map(int,input().split())
+v=[]
+visited=[0 for _ in range(N+1)]
+def go(idx,cnt):
+    if cnt==M:
+        print(*v)
+        return
+    for i in range(1,N+1):
+        v.append(i)
+        go(i,cnt+1)
+        v.pop()
+go(1,0)
