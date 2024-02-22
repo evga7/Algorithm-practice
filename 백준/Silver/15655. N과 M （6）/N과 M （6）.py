d4413da6b1f8689a00b9540e911c7a9e353cf5d3
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-02-22 19:11:36 +0900

    [Silver III] Title: N과 M (6), Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.py
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.py
@@ -1,16 +1,19 @@
 import sys
-def input():return sys.stdin.readline().rstrip()
+def input():return sys.stdin.readline().rstrip() 
 N,M=map(int,input().split())
+v=[]
 a=list(map(int,input().split()))
+visited=[0 for _ in range(N+1)]
 a.sort()
-v=[0 for _ in range(M)]
 def go(idx,cnt):
     if cnt==M:
         print(*v)
         return
-
     for i in range(idx,N):
-        v[cnt]=a[i]
+        if visited[i]:continue
+        v.append(a[i])
         go(i+1,cnt+1)
-        v[cnt]=0
+        v.pop()
 go(0,0)
+    
+    
