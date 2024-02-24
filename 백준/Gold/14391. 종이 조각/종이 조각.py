1249ee892f6c770ef5292a99c1801257af18f014
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-02-24 20:40:53 +0900

    [Gold III] Title: 종이 조각, Time: 164 ms, Memory: 110604 KB -BaekjoonHub

--- 백준/Gold/14391. 종이 조각/종이 조각.py
+++ 백준/Gold/14391. 종이 조각/종이 조각.py
@@ -1,38 +1,32 @@
 import sys
-def input(): return sys.stdin.readline().rstrip()
+def input():return sys.stdin.readline().rstrip()
 N,M=map(int,input().split())
 a=[list(map(int,input())) for _ in range(N)]
 res=0
-def go():
-    for cur in range(1<<(N*M)):
-        s=0
+def go(bit):
+    s=0
+    for i in range(N):
         ss=0
-        for i in range(N):
-            s+=ss
-            ss=0
-            for j in range(M):
-                idx=i*M+j
-                if cur& 1<<idx:
-                    ss=ss*10+a[i][j]
-                else:
-                    s+=ss
-                    ss=0
-        if ss:
-            s+=ss
-            ss=0
-        for i in range(M):
-            s+=ss
-            ss=0
-            for j in range(N):
-                idx=j*M+i
-                if not (cur& 1<<idx):
-                    ss=ss*10+a[j][i]
-                else:
-                    s+=ss
-                    ss=0
-        if ss:
-            s+=ss
-        global res
-        res=max(res,s)
-go()
+        for j in range(M):
+            idx=i*M+j
+            if bit & (1<<idx):
+                ss=ss*10+a[i][j]
+            else:
+                s += ss
+                ss = 0
+        s += ss
+    for i in range(M):
+        ss=0
+        for j in range(N):
+            idx=j*M+i
+            if bit & (1<<idx):
+                s+=ss
+                ss=0
+            else:
+                ss=ss*10+a[j][i]
+        s+=ss
+    return s
+    
+for i in range(1<<(N*M)):
+    res=max(res,go(i))
 print(res)
