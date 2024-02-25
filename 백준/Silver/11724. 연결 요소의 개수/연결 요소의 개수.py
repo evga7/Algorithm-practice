d6431cf1adddc6c019bcd15c3758b8c53632d365
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-02-25 20:58:23 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 256 ms, Memory: 110848 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,15 +1,14 @@
 import sys
-#sys.setrecursionlimit(200000)
+
+#sys.setrecursionlimit(10000)
 def input():return sys.stdin.readline().rstrip()
-INF=sys.maxsize
 N,M=map(int,input().split())
 p=[i for i in range(N+1)]
 def find(x):
-    if p[x]==x:
+    if x==p[x]:
         return x
     p[x]=find(p[x])
     return p[x]
-
 def uni(x,y):
     x=find(x)
     y=find(y)
@@ -23,9 +22,7 @@ def uni(x,y):
 for i in range(M):
     a,b=map(int,input().split())
     uni(a,b)
-res=1
-x=find(1)
-for i in range(2,N+1):
-    if uni(x,i):
-        res+=1
-print(res)
+st=set()
+for i in range(1,N+1):
+    st.add(find(i))
+print(len(st))
