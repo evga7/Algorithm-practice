e05d1ad38b29417eba1d5a7eea7edbf556717824
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-02-25 20:31:26 +0900

    [Silver II] Title: DFS와 BFS, Time: 172 ms, Memory: 126488 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,33 +1,34 @@
 from collections import *
 import sys
+sys.setrecursionlimit(10000)
 def input():return sys.stdin.readline().rstrip()
-N,M,S=map(int,input().split())
+N,M,V=map(int,input().split())
 g=[[] for _ in range(N+1)]
+visited=[0 for _ in range(N+1)]
 for i in range(M):
     a,b=map(int,input().split())
     g[a].append(b)
     g[b].append(a)
-v=[0 for _ in range(N+1)]
-def go(cur):
+def dfs(cur):
     print(cur,end=' ')
+    visited[cur]=1
     for nxt in g[cur]:
-        if v[nxt]:continue
-        v[nxt]=1
-        go(nxt)
-for i in range(N+1):
-    g[i].sort()
-v[S]=1
-go(S)
-
-v=[0 for _ in range(N+1)]
-q=deque()
-q.append(S)
-v[S]=1
-print('')
-while q:
-    x=q.popleft()
-    print(x,end=' ')
-    for nxt in g[x]:
-        if not v[nxt]:
-            v[nxt]=1
+        if visited[nxt]:continue
+        dfs(nxt)
+def bfs(start):
+    q=deque()
+    q.append(start)
+    visited[start]=1
+    while q:
+        cur=q.popleft()
+        print(cur,end=' ')
+        for nxt in g[cur]:
+            if visited[nxt]:continue
+            visited[nxt]=1
             q.append(nxt)
+for i in range(1,N+1):
+    g[i].sort()
+dfs(V)
+visited=[0 for _ in range(N+1)]
+print('')
+bfs(V)
