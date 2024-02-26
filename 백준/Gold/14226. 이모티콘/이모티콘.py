9950da5f622d81a6833149f5545324609d535e5a
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-02-27 00:14:51 +0900

    [Gold IV] Title: 이모티콘, Time: 300 ms, Memory: 126740 KB -BaekjoonHub

--- 백준/Gold/14226. 이모티콘/이모티콘.py
+++ 백준/Gold/14226. 이모티콘/이모티콘.py
@@ -1,51 +1,25 @@
-# N,M=map(int,input().split())
-# visited=[[987654321]*M for _ in range(N)]
-# arr=[list(map(int,input().strip())) for _ in range(N)]
-# str=re.findall(r'\d+',str) 숫자만 때기
-# re.findall(r'-?\d+', str1)] 음수는 이렇게
-# word1 = re.findall("[a-zA-Z]+", st) 이건 문자 추출
-#arr2 = [k[::-1] for k in zip(*arr)] 문자열 90도 회전
-import bisect
-import math
-import re
-
-import itertools
-import collections
-#sys.setrecursionlimit(10 ** 6)
-from collections import *
-from functools import cmp_to_key
-#dx = [0,-1,0,1]  # 오 위 왼 아
-#dy = [1,0,-1,0]
-dx=[1,1,1,0,0,0,-1,-1,-1]
-dy=[-1,0,1,-1,0,1,-1,0,1]
 import heapq
 def is_inside(x,y,N,M):
     return 0<=x<N and 0<=y<M
+from collections import *
 import sys
+#sys.setrecursionlimit(10000)
 def input():return sys.stdin.readline().rstrip()
-INF=sys.maxsize
-
-
-def is_inside(x, y, N, M):
-    return 0 <= x < N and 0 <= y < M
 N=int(input())
-st=set()
-q=deque()
+visited=[[0 for _ in range(1001)] for _ in range(1001)]
+q=[]
 q.append((0,1,0))
 while q:
-    cost,pos,cpy=q.popleft()
-    if pos==N:
+    cost,x,copy=heapq.heappop(q)
+    if x==N:
         print(cost)
         exit(0)
-    if pos+cpy<=1000 and not (pos+cpy,cpy) in st:
-        st.add((pos+cpy,cpy))
-        q.append((cost+1,pos+cpy,cpy))
-    if pos-1>=2 and not pos -1 in st:
-        st.add(pos -1)
-        q.append((cost + 1, pos -1,cpy))
-    if not (pos,pos) in st:
-        st.add((pos,pos))
-        q.append((cost+1,pos,pos))
-    
-
-    
+    if x>copy and not visited[x][x]:
+        visited[x][x]=1
+        heapq.heappush(q,(cost+1,x,x))
+    if x+copy<=1000 and not visited[x][x+copy]:
+        visited[x][x+copy]=1
+        heapq.heappush(q,(cost+1,x+copy,copy))
+    if x-1>=0 and not visited[x-1][copy]:
+        visited[x-1][copy]=1
+        heapq.heappush(q,(cost+1,x-1,copy))
