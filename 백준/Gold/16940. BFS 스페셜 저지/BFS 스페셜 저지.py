91b4d1c4b7332e071d1d4d91592ec0c69e2ef9ea
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-02-26 20:38:25 +0900

    [Gold III] Title: BFS 스페셜 저지, Time: 336 ms, Memory: 164944 KB -BaekjoonHub

---
+++ 백준/Gold/16940. BFS 스페셜 저지/BFS 스페셜 저지.py
@@ -0,0 +1,28 @@
+from collections import *
+import sys
+#sys.setrecursionlimit(10000)
+def input():return sys.stdin.readline().rstrip()
+N=int(input())
+g=[set() for _ in range(N+1)]
+v=[]
+visited=[0 for _ in range(N+1)]
+for i in range(N-1):
+    a,b=map(int,input().split())
+    g[a].add(b)
+    g[b].add(a)
+a=list(map(int,input().split()))
+if a[0]!=1:
+    print(0)
+    exit(0)
+idx=1
+q=deque()
+q.append(1)
+while q:
+    x=q.popleft()
+    while idx<N and a[idx] in g[x]:
+        q.append(a[idx])
+        idx+=1
+if idx==N:
+    print(1)
+else:
+    print(0)
