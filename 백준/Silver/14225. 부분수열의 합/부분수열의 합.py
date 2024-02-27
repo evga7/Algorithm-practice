0571ce724234db199391c1d46a3b84edef5a557e
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-02-28 01:09:30 +0900

    [Silver I] Title: 부분수열의 합, Time: 112 ms, Memory: 236084 KB -BaekjoonHub

--- 백준/Silver/14225. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/14225. 부분수열의 합/부분수열의 합.py
@@ -1,18 +1,13 @@
 import sys
-#sys.setrecursionlimit(100000)
-def input():return sys.stdin.readline().rstrip()
-INF=sys.maxsize
+sys.setrecursionlimit(100000)
+def input(): return sys.stdin.readline().rstrip()
 N=int(input())
 a=list(map(int,input().split()))
-st=set()
-def go(idx,s):
-    if idx>=N:
-        st.add(s)
-        return
-    go(idx+1,s+a[idx])
-    go(idx+1,s)
-go(0,0)
-for i in range(1,2000001):
-    if i in st:continue
-    print(i)
-    exit(0)
+res=0
+a.sort()
+s=0
+for i in range(N):
+    if s+1<a[i]:
+        break
+    s+=a[i]
+print(s+1)
