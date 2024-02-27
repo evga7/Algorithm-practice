36db453d5ee9a95be0022e07f0acd631c21f6ff0
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-02-27 22:24:29 +0900

    [Gold IV] Title: 트리의 지름, Time: 156 ms, Memory: 244396 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.py
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.py
@@ -1,40 +1,28 @@
-from collections import *
 import sys
-def input():return sys.stdin.readline().rstrip()
-N=int(input())
-g=[[] for _ in range(N+1)]
-r=0
+sys.setrecursionlimit(100000)
+def input(): return sys.stdin.readline().rstrip()
+N = int(input())
+g = [[] for _ in range(N + 1)]
+visited = [0 for _ in range(N + 1)]
+root = 1
+res = 0
 for i in range(N-1):
     a,b,c=map(int,input().split())
     g[a].append((b,c))
     g[b].append((a,c))
-dist=[0 for _ in range(N+1)]
-q=deque()
-q.append((1,0))
-s=0
-dist[1]=1
-while q:
-    x,cost=q.popleft()
-    for cur in g[x]:
-        nxt,c=cur[0],cur[1]
-        if not dist[nxt]:
-            dist[nxt]=cost+c
-            q.append((nxt,cost+c))
-idx=0
-for i in range(1,N+1):
-    if dist[i]>r:
-        r=dist[i]
-        idx=i
-q.append((idx,0))
-dist=[0 for _ in range(N+1)]
-res=0
-dist[idx]=1
-while q:
-    x,cost=q.popleft()
-    res=max(res,cost)
-    for cur in g[x]:
-        nxt,c=cur[0],cur[1]
-        if not dist[nxt]:
-            dist[nxt]=cost+c
-            q.append((nxt,cost+c))
+
+def go(cur, cnt):
+    if visited[cur]: return
+    visited[cur] = 1
+    global res, root
+    if res < cnt:
+        res = cnt
+        root = cur
+    for x in g[cur]:
+        nxt,cost=x[0],x[1]
+        if visited[nxt]: continue
+        go(nxt, cnt + cost)
+go(1, 0)
+visited = [0 for _ in range(N + 1)]
+go(root, 0)
 print(res)
