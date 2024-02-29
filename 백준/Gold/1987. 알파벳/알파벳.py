b6a1231efabb37315b13d3e55650759155f94291
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-02-29 21:31:15 +0900

    [Gold IV] Title: 알파벳, Time: 704 ms, Memory: 397796 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,29 +1,36 @@
-dx = [1, -1, 0, 0]
-dy = [0, 0, 1, -1]
+dx = [-1,1,0,0]  # 위 아 오 왼
+dy = [0,0,1,-1]
+# dx = [-1,-2,-2,-1,1,2,2,1]  # 오 왼 위 아
+# dy = [-2,-1,1,2,2,1,-1,-2]
+import heapq
+def is_inside(x,y,N,M):
+    return 0<=x<N and 0<=y<M
 from collections import *
-def is_inside(x, y, N, M):
-    return 0 <= x < N and 0 <= y < M
 import sys
+sys.setrecursionlimit(100000)
 def input(): return sys.stdin.readline().rstrip()
-def cov(s):
-    return ord(s)-ord('A')
 N,M=map(int,input().split())
 a=[list(input()) for _ in range(N)]
 st=set()
-q=deque()
-res=0
-q.append((0,0,1,1<<(cov(a[0][0]))))
-while q:
-    x,y,cost,bit=q.popleft()
-    res=max(res,cost)
-    for i in range(4):
-        n_x=x+dx[i]
-        n_y=y+dy[i]
-        if is_inside(n_x,n_y,N,M):
-            chk_bit=cov(a[n_x][n_y])
-            if not (bit & (1<<chk_bit)):
-                n_bit=bit|(1<<chk_bit)
-                if not (n_x,n_y,n_bit) in st:
-                    q.append((n_x,n_y,cost+1,n_bit))
-                    st.add((n_x,n_y,n_bit))
-print(res)
+def cal(c):
+    return ord(c)-ord('A')
+def go():
+    q=deque()
+    q.append((0,0,1<<cal(a[0][0]),1))
+    r=0
+    st.add((0,0,1<<cal(a[0][0])))
+    while q:
+        x,y,bit,cnt=q.popleft()
+        r=max(r,cnt)
+        for i in range(4):
+            n_x=x+dx[i]
+            n_y=y+dy[i]
+            if is_inside(n_x,n_y,N,M):
+                n_bit=1<<cal(a[n_x][n_y])
+                n_bit2=bit|n_bit
+                if not bit & n_bit and not (n_x,n_y,n_bit2) in st:
+                    q.append((n_x,n_y,n_bit2,cnt+1))
+                    st.add((n_x,n_y,n_bit2))
+    return r
+        
+print(go())
