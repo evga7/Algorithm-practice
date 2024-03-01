b8dd9ae2a09487cf63d91c6c6cd6c30efd13fdff
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-03-01 22:15:26 +0900

    [Silver I] Title: 행렬, Time: 124 ms, Memory: 111248 KB -BaekjoonHub

--- 백준/Silver/1080. 행렬/행렬.py
+++ 백준/Silver/1080. 행렬/행렬.py
@@ -1,30 +1,23 @@
 import sys
 #sys.setrecursionlimit(100000)
-def input():return sys.stdin.readline().rstrip()
-INF=sys.maxsize
+def input(): return sys.stdin.readline().rstrip()
 N,M=map(int,input().split())
 a=[list(map(int,input())) for _ in range(N)]
 b=[list(map(int,input())) for _ in range(N)]
-res=0
-def d():
-    for i in range(N):
-        for j in range(M):
-            if a[i][j]!=b[i][j]:
-                return False
-    return True
-def f(x,y):
+def go(x,y):
     for i in range(x,min(x+3,N)):
         for j in range(y,min(y+3,M)):
             a[i][j]=1-a[i][j]
-if d():
-    print(res)
-    exit(0)
-for i in range(N-2):
-    for j in range(M-2):
+res=0
+if N>=3 and M>=3:
+    for i in range(N-2):
+        for j in range(M-2):
+            if a[i][j]!=b[i][j]:
+                go(i,j)
+                res+=1
+for i in range(N):
+    for j in range(M):
         if a[i][j]!=b[i][j]:
-            f(i,j)
-            res+=1
-            if d():
-                print(res)
-                exit(0)
-print(-1)
+            print(-1)
+            exit(0)
+print(res)
