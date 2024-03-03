fd7eb6f97f4c27a72de97f0ac5a1b4d96a1dc420
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-03-03 23:39:21 +0900

    [Gold V] Title: 전구와 스위치, Time: 140 ms, Memory: 119604 KB -BaekjoonHub

--- 백준/Gold/2138. 전구와 스위치/전구와 스위치.py
+++ 백준/Gold/2138. 전구와 스위치/전구와 스위치.py
@@ -1,25 +1,24 @@
 import sys
-def input():return sys.stdin.readline().rstrip()
+#sys.setrecursionlimit(100000)
+def input(): return sys.stdin.readline().rstrip()
 N=int(input())
 s=list(map(int,input()))
 s2=list(map(int,input()))
-res=987654321
-def go(s1,b):
-    cnt=0
-    a=s1[:]
-    for i in range(1,len(a)):
-        if a[i-1]==b[i-1]:continue
-        cnt+=1
-        for j in range(i-1,min(i + 2, len(a))):
-            a[j] ^= 1
-    if a==b:
+def go(start,cnt,op):
+    temp=s[:]
+    if op:
+        temp[0]=1-temp[0]
+        temp[1]=1-temp[1]
+    for i in range(start,N):
+        if temp[i-1]!=s2[i-1]:
+            cnt+=1
+            for j in range(i-1,min(N,i+2)):
+                temp[j]=1-temp[j]
+    if temp==s2:
         return cnt
-    else:
-        return 987654321
-res=min(res,go(s,s2))
-s[0]^=1
-s[1]^=1
-res=min(res,go(s,s2)+1)
+    return 987654321
+res=987654321
+res=min(res,go(1,0,0),go(1,1,1))
 if res==987654321:
     res=-1
 print(res)
