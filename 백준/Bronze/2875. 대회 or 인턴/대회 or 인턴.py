801c7b73128ca321229a27b0e60fb3bc44a8e16a
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-03-13 20:21:04 +0900

    [Bronze III] Title: 대회 or 인턴, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/2875. 대회 or 인턴/대회 or 인턴.py
@@ -0,0 +1,10 @@
+import sys
+# sys.setrecursionlimit(100000)
+def input(): return sys.stdin.readline().rstrip()
+N,M,K=map(int,input().split())
+for i in range(K):
+    if N//2>=M:
+        N-=1
+    else:
+        M-=1
+print(min(N//2,M))
