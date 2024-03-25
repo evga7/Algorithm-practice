343a5149f71f526f66c760ed3af42b9f77cd180a
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-03-25 18:04:07 +0900

    [Gold II] Title: 골목 대장 호석 - 효율성 2, Time: 4688 ms, Memory: 228308 KB, Score: 39 point -BaekjoonHub

--- 백준/Gold/20183. 골목 대장 호석 － 효율성 2/골목 대장 호석 － 효율성 2.py
+++ 백준/Gold/20183. 골목 대장 호석 － 효율성 2/골목 대장 호석 － 효율성 2.py
@@ -1,46 +1,43 @@
 import heapq
-def is_inside(x,y,N,M):
-    return 0<=x<=N and 0<=y<=M
-
+def is_inside(x, y, N, M):
+    return 0 <= x < N and 0 <= y < M
+import copy
+from collections import *
+import bisect
+# sys.setrecursionlimit(100000)
 import sys
-#sys.setrecursionlimit(100000)
-def input():return sys.stdin.readline().rstrip()
-INF=sys.maxsize
+def input():
+    return sys.stdin.readline().rstrip()
 N,M,A,B,C=map(int,input().split())
 g=[[] for _ in range(N+1)]
 for i in range(M):
     a,b,c=map(int,input().split())
     g[a].append((b,c))
     g[b].append((a,c))
-
-def go(mid):
-    q=[]
-    q.append((0,C,A))
-    dist = [int(1e15) for _ in range(N + 1)]
-    dist[A] = 0
-    visited = [0 for _ in range(N + 1)]
-    while q:
-        cost,money,pos=heapq.heappop(q)
-        if pos==B:
-            return cost
-        if visited[pos] or cost>mid: continue
-        visited[pos]=1
-        for nxt,go_cost in g[pos]:
-            n_cost=max(go_cost,cost)
-            if dist[nxt]>n_cost and money-go_cost>=0:
-                dist[nxt]=n_cost
-                heapq.heappush(q,(n_cost,money-go_cost,nxt))
-    return -1
-        
 left=0
-right=int(1e9)
-res=-1
+right=C
+def go(mid):
+    dist=[9876543210 for _ in range(N+1)]
+    q=[]
+    q.append((0,A))
+    while q:
+        cost,cur=heapq.heappop(q)
+        if dist[cur]<cost:continue
+        if cur==B:
+            return True
+        for x in g[cur]:
+            nxt,n_cost=x[0],x[1]+cost
+            if x[1]<=mid and n_cost<=C and dist[nxt]>n_cost :
+                dist[nxt]=n_cost
+                heapq.heappush(q,(n_cost,nxt))
+res=987654321
 while left<=right:
     mid=left+right>>1
-    c=go(mid)
-    if c>0:
-        res=c
+    if go(mid):
         right=mid-1
+        res=min(res,mid)
     else:
         left=mid+1
+if res==987654321:
+    res=-1
 print(res)
