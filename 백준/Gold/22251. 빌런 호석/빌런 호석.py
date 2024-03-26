2ff4eb0e6e42f82838e97567072f1a37fe037e1d
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-03-26 16:19:19 +0900

    [Gold V] Title: 빌런 호석, Time: 592 ms, Memory: 111784 KB -BaekjoonHub

--- 백준/Gold/22251. 빌런 호석/빌런 호석.py
+++ 백준/Gold/22251. 빌런 호석/빌런 호석.py
@@ -1,20 +1,29 @@
-import sys
-from collections import *
-def input():return sys.stdin.readline().rstrip()
 N,K,P,X=map(int,input().split())
-op=[63,6,91,79,102,109,125,7,127,111]
-X=deque(str(X))
+p=[63,6,91,79,102,109,125,7,127,111]
+def chk(a,b):
+    al=len(a)
+    bl=len(b)
+    if len(a)<len(b):
+        a=(bl-al)*'0'+a
+    elif len(a)>len(b):
+        b = (al- bl) * '0' + b
+    cnt=0
+    l=max(al,bl)
+    for i in range(l):
+        if b[i]!=a[i]:
+            bi=int(b[i])
+            ai=int(a[i])
+            for j in range(7):
+                if (p[bi] & 1<<j)^(p[ai] & 1<<j):
+                    cnt+=1
+                if cnt>P:
+                    return cnt
+    return cnt
+                
 res=0
 for i in range(1,N+1):
-    num=deque(str(i))
-    cnt=0
-    while len(num)<len(X):
-        num.appendleft('0')
-    while len(num)>len(X):
-       X.appendleft('0')
-    for j in range(len(X)):
-        cnt+=bin(op[int(num[j])]^op[int(X[j])])[2:].count('1')
-        if cnt>P:break
-    if 1<=cnt<=P:
+    s=chk(str(X),str(i))
+    if 1<=s<=P:
         res+=1
+    
 print(res)
