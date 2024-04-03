14715d799b1fb6557aa30a047d2228ee085d8c2c
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-04-03 14:01:25 +0900

    [Gold III] Title: 앱, Time: 328 ms, Memory: 123192 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.py
+++ 백준/Gold/7579. 앱/앱.py
@@ -1,23 +1,20 @@
 N,M=map(int,input().split())
 a=list(map(int,input().split()))
 b=list(map(int,input().split()))
-mid=0
 def go(idx,num):
     if idx==N:
         return 0
     if dp[idx][num]!=-1:
         return dp[idx][num]
     ret=0
-    for i in range(idx,N):
-        if num-b[i]>=0:
-            ret=max(ret,go(i+1,num-b[i])+a[i])
+    if num-b[idx]>=0:
+        ret=max(ret,go(idx+1,num-b[idx])+a[idx])
+    ret=max(ret,go(idx+1,num))
     dp[idx][num]=ret
     return dp[idx][num]
 res=0
 dp = [[-1 for _ in range(10001)] for _ in range(N + 1)]
 for i in range(10001):
-    mid=i
     if go(0,i)>=M:
         print(i)
         exit(0)
-        
