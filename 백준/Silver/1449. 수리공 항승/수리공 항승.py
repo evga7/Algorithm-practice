df9ebab1053da0b7f68dbe8357af1caa317ee222
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-04-05 14:03:59 +0900

    [Silver III] Title: 수리공 항승, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/1449. 수리공 항승/수리공 항승.py
+++ 백준/Silver/1449. 수리공 항승/수리공 항승.py
@@ -1,12 +1,10 @@
-import sys
-def input():return sys.stdin.readline().rstrip()
 N,M=map(int,input().split())
 a=list(map(int,input().split()))
 a.sort()
-res=0
-idx=0
-for cur in a:
-    if idx>=cur:continue
-    idx=cur+M-1
+back=a[0]
+res=1
+for i in range(1,N):
+    if a[i]-back<M:continue
     res+=1
+    back=a[i]
 print(res)
