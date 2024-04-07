c1ddac1688e73e5a497726fffc0ffea000f76e8c
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-04-07 21:47:12 +0900

    [Silver III] Title: 바이러스, Time: 116 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,23 +1,27 @@
-from collections import *
-import sys
-def input():return sys.stdin.readline().rstrip()
+def find(x):
+    if x==p[x]:
+        return x
+    p[x]=find(p[x])
+    return p[x]
+def uni(x,y):
+    x=find(x)
+    y=find(y)
+    if x!=y:
+        if x<y:
+            p[y]=x
+        else:
+            p[x]=y
+        return True
+    return False
 N=int(input())
 M=int(input())
-g=[[] for _ in range(N+1)]
-for _ in range(M):
+p=[i for i in range(N+1)]
+for i in range(M):
     a,b=map(int,input().split())
-    g[a].append(b)
-    g[b].append(a)
-q=deque()
-q.append(1)
-v=[0 for _ in range(N+1)]
-v[1]=1
-res=0
-while q:
-    cur=q.popleft()
-    for nxt in g[cur]:
-        if not v[nxt]:
-            v[nxt]=1
-            res+=1
-            q.append(nxt)
+    uni(a,b)
+c=find(1)
+res=N-1
+for i in range(1,N+1):
+    if c!=find(i):
+        res-=1
 print(res)
