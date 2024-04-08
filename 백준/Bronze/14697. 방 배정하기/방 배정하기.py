44f17c73dbaaf1fe2fa4c91b3bb035669eb9b09e
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-04-08 16:34:58 +0900

    [Bronze II] Title: 방 배정하기, Time: 120 ms, Memory: 109256 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/14697. 방 배정하기/방 배정하기.py
+++ 백준/Bronze/14697. 방 배정하기/방 배정하기.py
@@ -1,14 +1,15 @@
 *a,d=list(map(int,input().split()))
-idx=2
-while d:
-    if a[idx]>d:
-        idx-=1
-        if idx==-1:
-            break
-        continue
-    c=d//a[idx]
-    d-=a[idx]*c
-if d:
-    print(0)
-else:
-    print(1)
+dp=[-1 for _ in range(d+1)]
+def go(idx):
+    if idx==0:
+        return 1
+    if dp[idx]!=-1:
+        return dp[idx]
+    ret=0
+    for cur in a:
+        c=idx//cur
+        for i in range(1,c+1):
+            ret=max(ret,go(idx-(cur*i)))
+    dp[idx]=ret
+    return dp[idx]
+print(go(d))
