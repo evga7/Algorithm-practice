a26f28f784b63c0e8f1998fe06bd482affd03a5b
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-04-11 18:33:45 +0900

    [Gold IV] Title: N으로 만들기, Time: 132 ms, Memory: 112004 KB -BaekjoonHub

--- 백준/Gold/17255. N으로 만들기/N으로 만들기.py
+++ 백준/Gold/17255. N으로 만들기/N으로 만들기.py
@@ -1,20 +1,29 @@
 from collections import *
+#sys.setrecursionlimit(100000)
+dx = [1, -1, 0, 0]  # 아 위 오 왼
+dy = [0, 0, 1, -1]
+def is_inside(x, y, N, M):
+    return 0 <= x < N and 0 <= y < M
+
+
 import sys
 def input():return sys.stdin.readline().rstrip()
-S=input()
-st=set()
+N=input()
 q=deque()
-q.append((S,S,len(S)-1))
+st=set()
 res=0
+q.append((N,N,len(N)))
 while q:
-    cur,sts,cost=q.popleft()
-    if cost==0:
-        st.add(sts)
+    cur,t,l=q.popleft()
+    if l==1:
         res+=1
         continue
-    for nxt in [cur[1:],cur[:len(cur)-1]]:
-        nxt_sts=sts+nxt
-        if nxt and not nxt_sts in st:
-            q.append((nxt,nxt_sts,cost-1))
-            st.add(nxt_sts)
+    left=cur[1:]
+    right=cur[:l-1]
+    if not t+left in st:
+        st.add(t+left)
+        q.append((left,t+left,l-1))
+    if not t+right in st:
+        st.add(t+right)
+        q.append((right,t+right,l-1))
 print(res)
