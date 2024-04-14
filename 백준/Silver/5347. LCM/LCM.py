a34f271b9de337741f9199c8772c6a99d1a5b387
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-04-15 01:00:23 +0900

    [Silver V] Title: LCM, Time: 192 ms, Memory: 117276 KB -BaekjoonHub

---
+++ 백준/Silver/5347. LCM/LCM.py
@@ -0,0 +1,32 @@
+# N,M=map(int,input().split())
+# visited=[[987654321]*M for _ in range(N)]
+# arr=[list(map(int,input().strip())) for _ in range(N)]
+# str=re.findall(r'\d+',str) 숫자만 때기
+# re.findall(r'-?\d+', str1)] 음수는 이렇게
+# word1 = re.findall("[a-zA-Z]+", st) 이건 문자 추출
+#arr2 = [k[::-1] for k in zip(*arr)] 문자열 90도 회전
+import bisect
+import math
+import re
+import heapq
+import sys
+from collections import *
+import itertools
+import collections
+si=sys.stdin.readline
+#sys.setrecursionlimit(10 ** 6)
+dx = [0,-1,0,1]  # 왼 위 오 아래
+dy = [-1,0,1,0]
+def input():return sys.stdin.readline().rstrip()
+def gcd(x,y):
+    while y:
+        temp=x
+        x=y
+        y=temp%y
+
+    return x
+
+N=int(input())
+for i in range(N):
+    a,b=map(int,input().split())
+    print(a*b//gcd(a,b))
