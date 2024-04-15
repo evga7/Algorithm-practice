4c91b79b8f2f6aabd7b603ae0110eaded7224af6
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-04-15 18:18:51 +0900

    [Gold V] Title: 백도어, Time: 712 ms, Memory: 159012 KB -BaekjoonHub

--- 백준/Gold/17396. 백도어/백도어.py
+++ 백준/Gold/17396. 백도어/백도어.py
@@ -1,37 +1,35 @@
 import heapq
-import itertools
-import collections
-#sys.setrecursionlimit(10 ** 6)
-import sys
 from collections import *
-dx = [0,-1,0,1]  # 오 위 왼 아
-dy = [1,0,-1,0]
-def is_inside(x,y,N,M):
-    return 0<x<=N and 0<y<=M
-def input():return sys.stdin.readline().rstrip()
-INF=sys.maxsize
-N,M=map(int,input().split())
-dp=[-1 for _ in range(N+1)]
-g=[[] for _ in range(N+1)]
-arr=list(map(int,input().split()))
-arr[N-1]=0
-dist=[INF for _ in range(N+1)]
-for i in range(M):
-    a,b,c=map(int,input().split())
-    g[a].append((b,c))
-    g[b].append((a,c))
-def go():
-    q=[]
-    q.append((0,0))
-    while q:
-        cost,cur=heapq.heappop(q)
-        if dist[cur]<cost:continue
-        if cur==N-1:
-            return cost
-        for nxt,co in g[cur]:
-            if dist[nxt]>cost+co and not arr[nxt]:
-                dist[nxt]=cost+co
-                heapq.heappush(q,(cost+co,nxt))
-    return -1
+#sys.setrecursionlimit(100000)
+dx = [1, -1, 0, 0]  # 아 위 오 왼
+dy = [0, 0, 1, -1]
+def is_inside(x, y, N, M):
+    return 0 <= x < N and 0 <= y < M
 
-print(go())
+import math
+import sys
+def input():return sys.stdin.readline().rstrip()
+MAX=sys.maxsize
+N,M=map(int,input().split())
+a=list(map(int,input().split()))
+a[N-1]=0
+g=[[] for _ in range(N+1)]
+for i in range(M):
+    q,w,e=map(int,input().split())
+    g[q].append((w,e))
+    g[w].append((q,e))
+dist=[MAX for _ in range(N+1)]
+q=[]
+q.append((0,0))
+while q:
+    cost,cur=heapq.heappop(q)
+    if dist[cur]<cost:continue
+    if cur==N-1:
+        print(cost)
+        exit(0)
+    for x in g[cur]:
+        nxt,n_cost=x[0],cost+x[1]
+        if not a[nxt] and dist[nxt]>n_cost:
+            dist[nxt]=n_cost
+            heapq.heappush(q,(n_cost,nxt))
+print(-1)
