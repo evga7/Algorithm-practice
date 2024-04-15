415ed324e99aaf209369cd343eae84dc291c5a33
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-04-15 17:24:00 +0900

    [Gold IV] Title: 민서의 응급 수술, Time: 296 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Gold/20955. 민서의 응급 수술/민서의 응급 수술.py
+++ 백준/Gold/20955. 민서의 응급 수술/민서의 응급 수술.py
@@ -1,8 +1,5 @@
 import sys
-#sys.setrecursionlimit(100000)
 def input():return sys.stdin.readline().rstrip()
-#map(int,input().split())
-INF=sys.maxsize
 N,M=map(int,input().split())
 p=[i for i in range(N+1)]
 def find(x):
@@ -25,8 +22,8 @@ for i in range(M):
     a,b=map(int,input().split())
     if not uni(a,b):
         res+=1
-
-st=set()
-for i in range(1,N+1):
-    st.add(find(i))
-print(res+len(st)-1)
+num=find(1)
+for i in range(2,N+1):
+    if uni(num,i):
+        res+=1
+print(res)
