d5ed7c50a9ef67efeabe8c00fb74827c076fedc7
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-04-16 14:05:06 +0900

    [Gold V] Title: 수 고르기, Time: 164 ms, Memory: 111712 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.py
+++ 백준/Gold/2230. 수 고르기/수 고르기.py
@@ -1,17 +1,17 @@
 import sys
 def input():return sys.stdin.readline().rstrip()
+MAX=sys.maxsize
 N,M=map(int,input().split())
 a=[int(input()) for _ in range(N)]
+res=MAX
 a.sort()
 left=0
 right=1
-res=int(2e10)
-while left<N and right<N:
-    s=abs(a[right]-a[left])
+while left<=right and right<N:
+    s=a[right]-a[left]
     if s>=M:
         res=min(res,s)
         left+=1
     else:
         right+=1
-
 print(res)
