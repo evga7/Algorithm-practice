6bc9b230e64e1bedde315e847f7bc83efc080ea8
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-05-04 15:00:53 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.py
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.py
@@ -1,18 +1,11 @@
 def solution(common):
     answer = 0
     s=common[1]-common[0]
-    m=-987654321
-    if common[0]!=0:
-        m=common[1]//common[0]
     l=len(common)
     for i in range(2,l):
         if common[i]-common[i-1]!=s:
             s=-987654321
-        if common[i-1]==0:continue
-        if common[i]//common[i-1]!=m:
-            m=-987654321
+            break
     if s!=-987654321:
         return common[l-1]+s
-    if m!=-987654321:
-        return common[l-1]*m
-    return answer
+    return common[l-1]*(common[1]//common[0])
