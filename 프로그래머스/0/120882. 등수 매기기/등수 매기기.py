2bfa9888887cf8bf721789b76f4c9a3b7ff4fed5
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-05-05 17:02:01 +0900

    [level 0] Title: 등수 매기기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120882. 등수 매기기/등수 매기기.py
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.py
@@ -2,20 +2,9 @@ def solution(score):
     answer = []
     v=[]
     l=len(score)
-    v2=[0 for _ in range(l)]
     for i,cur in enumerate(score):
-        v.append((cur[0]+cur[1],i))
+        v.append(cur[0]+cur[1])
     v.sort(reverse=True)
-    idx=1
-    cnt=0
-    for i in range(l):
-        q,w=v[i][0],v[i][1]
-        v2[w]=idx
-        if i+1<l and v[i][0]==v[i+1][0]:
-            cnt+=1
-            continue
-        else:
-            idx+=cnt
-            cnt=0
-        idx+=1+cnt
-    return v2[:l]
+    for cur in score:
+        answer.append(v.index(sum(cur))+1)
+    return answer
