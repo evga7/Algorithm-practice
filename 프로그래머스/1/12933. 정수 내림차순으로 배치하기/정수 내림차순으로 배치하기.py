fe32ef9e2ecbca457366adada11dd89c5e8d5e6c
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-05-07 21:16:16 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.py
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.py
@@ -1,10 +1,2 @@
 def solution(n):
-    v=[]
-    while n:
-        v.append(n%10)
-        n//=10
-    v.sort(reverse=True)
-    answer=0
-    for cur in v:
-        answer=answer*10+cur
-    return answer
+    return int(''.join(sorted(str(n),reverse=True)))
