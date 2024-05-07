1da17a424584021bda18a509dd3925fba70e4ffc
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-05-08 02:44:14 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -2,13 +2,10 @@ def solution(n, arr1, arr2):
     answer = []
     for i in range(n):
         c,c2=arr1[i],arr2[i]
-        c=c|c2
-        temp=""
-        for j in range(n):
-            if c&(1<<j):
-                temp+='#'
-            else:
-                temp+=' '
-        answer.append(temp[::-1])
+        c=bin(c|c2)[2:]
+        c=c.rjust(n,"0")
+        c=c.replace('1','#')
+        c=c.replace('0',' ')
+        answer.append(c)
                 
     return answer
