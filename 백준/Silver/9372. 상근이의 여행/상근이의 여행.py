68689a65020621a747cdbd890106d8d10f6c0a7d
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-06-05 14:30:46 +0900

    [Silver IV] Title: 상근이의 여행, Time: 132 ms, Memory: 110272 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.py
@@ -0,0 +1,11 @@
+import sys
+def input(): return sys.stdin.readline().rstrip()
+MAX = sys.maxsize
+MOD = int(1e9) + 9
+T = int(input())
+while T:
+    T -= 1
+    N, M = map(int, input().split())
+    for i in range(M):
+        a, b = map(int, input().split())
+    print(N-1)
