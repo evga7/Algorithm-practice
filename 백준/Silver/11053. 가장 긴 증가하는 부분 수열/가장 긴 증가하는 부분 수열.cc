4bf35b1b6530c453103d0272a2eabcf3ae81e9ae
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-07-01 18:21:54 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 1996 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -1,47 +1,30 @@
-#include <bits/stdc++.h>
-#define MAX 987654321
-#define MIN -987654321
+#include <iostream>
+#include <algorithm>
+#include <string>
 using namespace std;
-typedef long long int ll;
-typedef pair<int, int> pi;
-typedef pair<int, string> pi3;
-typedef pair<int, pair<int, int>>pi2;
-typedef vector<vector<int>> vpi;
-typedef vector<int> vec;
-typedef vector<pi> vec2;
-typedef queue<pi> que;
-typedef vector<vector<pi>> gra;
-int dx[4] = { 0,1,0,-1 };
-int dy[4] = { 1,0,-1,0 };
-int N, M;
-int res;
-int num;
-vector<int>v;
+int arr[1001];
+int dp[1001];
 int main()
 {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
+	int N;
+	scanf("%d", &N);
 	int i, j;
-	cin >> N;
-	v.push_back(-MAX);
-	for (i = 0; i <N; i++)
+	int res=-1;
+	int min;
+	for (i = 0; i < N; i++)
+		scanf("%d", &arr[i]);
+	for (i = 0; i < N; i++)
 	{
-		cin >> num;
-		if (num > v.back())
+		min = 0;
+		for (j = 0; j <i; j++)
 		{
-			v.push_back(num);
-			res++;
+			if (arr[i] > arr[j]&&dp[j]>min)
+			{
+				min = dp[j];
+			}
 		}
-		else
-		{
-			auto it = lower_bound(v.begin(), v.end(), num);
-			*it = num;
-		}
+		dp[i] = min + 1;
+		res = max(dp[i], res);
 	}
-	//memset(dp, -1, sizeof(dp));
-	
-	cout << res;
+	printf("%d", res);
 }
-
-
