4cf16672edf6d5768b5b25515bd9c6a251b16093
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-07-02 11:14:52 +0900

    [Silver III] Title: 합 구하기, Time: 164 ms, Memory: 121628 KB -BaekjoonHub

--- 백준/Silver/11441. 합 구하기/합 구하기.py
+++ 백준/Silver/11441. 합 구하기/합 구하기.py
@@ -1,13 +1,13 @@
 import sys
-#sys.setrecursionlimit(200000)
 def input():return sys.stdin.readline().rstrip()
-INF=sys.maxsize
+MAX=sys.maxsize
+MOD=int(1e9)+9
 N=int(input())
 a=list(map(int,input().split()))
 M=int(input())
-s=[0 for _ in range(N+1)]
+b=[0 for _ in range(N+1)]
 for i in range(1,N+1):
-    s[i]=s[i-1]+a[i-1]
+    b[i]=b[i-1]+a[i-1]
 for i in range(M):
-    a,b=map(int,input().split())
-    print(s[b]-s[a-1])
+    s,e=map(int,input().split())
+    print(b[e]-b[s-1])
