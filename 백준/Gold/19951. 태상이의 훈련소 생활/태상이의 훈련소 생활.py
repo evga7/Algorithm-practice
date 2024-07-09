591d393e6d5d446ecc798227475ba5a7f937c340
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-07-09 13:41:27 +0900

    [Gold V] Title: 태상이의 훈련소 생활, Time: 236 ms, Memory: 129176 KB -BaekjoonHub

--- 백준/Gold/19951. 태상이의 훈련소 생활/태상이의 훈련소 생활.py
+++ 백준/Gold/19951. 태상이의 훈련소 생활/태상이의 훈련소 생활.py
@@ -1,17 +1,24 @@
-import sys
 from collections import *
 #sys.setrecursionlimit(100000)
+#dx = [1, -1, 0, 0]  # 아 위 오 왼
+#dy = [0, 0, 1, -1]
+def is_inside(x, y, N, M):
+    return 0 <= x < N and 0 <= y < M
+
+import math
+import sys
 def input():return sys.stdin.readline().rstrip()
-INF=sys.maxsize
+MAX=sys.maxsize
+MOD=int(1e9)+9
 N,M=map(int,input().split())
-arr=list(map(int,input().split()))
+a=list(map(int,input().split()))
 m=defaultdict(int)
 for i in range(M):
-    a,b,c=map(int,input().split())
-    m[b]-=c
-    m[a-1]+=c
-res=0
-c=0
+    q,w,e=map(int,input().split())
+    m[q-1]+=e
+    m[w]-=e
+s=0
 for i in range(N):
-    c+=m[i]
-    print(arr[i]+c,end=' ')
+    if i in m:
+        s+=m[i]
+    print(a[i]+s,end=' ')
