07e979cb0900cc1bc18bd2e0eaf2dc0dc184ed97
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-07-24 20:26:48 +0900

    [Gold IV] Title: 오렌지 출하, Time: 4404 ms, Memory: 266848 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/11985. 오렌지 출하/오렌지 출하.py
+++ 백준/Gold/11985. 오렌지 출하/오렌지 출하.py
@@ -1,25 +1,25 @@
 import sys
+sys.setrecursionlimit(100000)
 def input(): return sys.stdin.readline().rstrip()
 MAX = sys.maxsize
 MOD = int(1e9) + 9
 N,M,K=map(int,input().split())
 a=[int(input()) for _ in range(N)]
-dp=[-1 for _ in range(N+1)]
+dp=[MAX for _ in range(N+1)]
 def go(idx):
     if idx==N:
         return 0
-    if dp[idx]!=-1:
+    if dp[idx]!=MAX:
         return dp[idx]
     ret=MAX
-    mi=MAX
-    mx=0
+    mi=a[idx]
+    mx=a[idx]
     for i in range(idx,min(idx+M,N)):
         mx=max(mx,a[i])
         mi=min(mi,a[i])
         su=mx-mi
-        num=i-idx+1
-        ret=min(ret,go(i+1)+K+num*su)
+        num=(i-idx)+1
+        ret=min(ret,go(i+1)+(K+(num*su)))
     dp[idx]=ret
     return dp[idx]
 print(go(0))
-
