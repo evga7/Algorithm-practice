8aedb59b49040c37c43d565462b5a946314c0951
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-08-15 10:18:50 +0900

    [Silver IV] Title: 오셀로 재배치, Time: 528 ms, Memory: 230436 KB -BaekjoonHub

--- 백준/Silver/13413. 오셀로 재배치/오셀로 재배치.py
+++ 백준/Silver/13413. 오셀로 재배치/오셀로 재배치.py
@@ -1,31 +1,20 @@
+import sys
+# sys.setrecursionlimit(100000)
+def input(): return sys.stdin.readline().rstrip()
+#
+INF = sys.maxsize
 T=int(input())
-def chk():
-    cnt=0
-    w_cnt=0
-    b_cnt=0
-    for i in range(N):
-        if s[i]==s2[i]:continue
-        if s2[i]=='W':
-            if b_cnt:
-                b_cnt-=1
-                cnt+=1
-            else:
-                w_cnt+=1
-        elif s2[i]=='B':
-            if w_cnt:
-                w_cnt-=1
-                cnt+=1
-            else:
-                b_cnt+=1
-            
-    return cnt+w_cnt+b_cnt
-        
-        
-            
 while T:
     T-=1
     N=int(input())
     s=list(input())
-    s2=list(input())
-    print(chk())
-    
+    s2 = list(input())
+    w_cnt=0
+    b_cnt=0
+    for i in range(len(s)):
+        if s[i]!=s2[i]:
+            if s2[i]=='W':
+                w_cnt+=1
+            else:
+                b_cnt+=1
+    print(max(w_cnt,b_cnt))
