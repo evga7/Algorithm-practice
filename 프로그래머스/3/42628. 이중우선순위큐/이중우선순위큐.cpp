0199a0fa313e1eec95353ece43ba4ecaf77041ee
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-08-17 11:45:55 +0900

    [level 3] Title: 이중우선순위큐, Time: 15.95 ms, Memory: 10.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+multiset<int>mq;
+vector<int> solution(vector<string> operations) {
+    vector<int> answer;
+    for (auto cur : operations)
+    {
+        char op;
+        op=cur[0];
+        string s = cur.substr(2);
+        int num = stoi(s);
+        if (op=='I')
+        {
+            mq.insert(num);
+        }
+        else if (op=='D' && mq.size())
+        {
+            if (num==-1){
+                mq.erase(mq.begin());
+            }
+            else{
+                mq.erase(--mq.end());
+            }
+        }
+    }
+    if (mq.size())
+        return {*--mq.end(),*mq.begin()};
+    return {0,0};
+}
