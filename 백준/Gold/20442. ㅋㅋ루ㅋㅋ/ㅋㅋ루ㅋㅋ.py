8094e8376571e2819ad6202cb91ff14328d36fde
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-08-23 15:06:27 +0900

    [Gold II] Title: ㅋㅋ루ㅋㅋ, Time: 360 ms, Memory: 224800 KB -BaekjoonHub

--- 백준/Gold/20442. ㅋㅋ루ㅋㅋ/ㅋㅋ루ㅋㅋ.py
+++ 백준/Gold/20442. ㅋㅋ루ㅋㅋ/ㅋㅋ루ㅋㅋ.py
@@ -1,5 +1,3 @@
-from collections import *
-import bisect
 import sys
 #sys.setrecursionlimit(100000)
 def input(): return sys.stdin.readline().rstrip()
@@ -8,49 +6,27 @@ MOD = int(1e9) + 9
 s=input()
 res=s.count('R')
 l=len(s)
-l_s=[0 for _ in range(l)]
-r_s=[0 for _ in range(l)]
-R_s=[0 for _ in range(l+1)]
-if s[0]=='K':
-    l_s[0]=1
-if s[l-1]=='K':
-    r_s[l-1]=1
-if s[0]=='R':
-    R_s[0]=1
-for i in range(1,l):
-    l_s[i]=l_s[i-1]
-    r_s[l-i-1]=r_s[l-i]
-    R_s[i+1]=R_s[i]
-    if s[i]=='K':
-        l_s[i]+=1
+l_c=[]
+r_c=[]
+cnt=0
+for cur in s:
+    if cur=='K':
+        cnt+=1
     else:
-        R_s[i+1]+=1
-    if s[l-i-1]=='K':
-        r_s[l-i-1]+=1
-r_cnt=0
-l_idx=-1
-r_idx=-1
-l_m=defaultdict(int)
-r_m=defaultdict(int)
-for i in range(l):
-    if s[i]=='K':
-        if not l_s[i] in l_m:
-            l_m[l_s[i]]=i
-    if s[l-i-1]=='K':
-        if not r_s[l-i-1] in r_m:
-            r_m[r_s[l-i-1]]=l-i-1
-l_cnt=0
-for i in range(l):
-    if s[i]=='R':
-        mi=min(l_s[i],r_s[i])
-        if mi:
-            l_idx=l_m[mi]
-            r_idx = r_m[mi]
-            res=max(res,(R_s[r_idx]-R_s[l_idx])+mi*2)
-    elif s[i]=='K':
-        l_cnt+=1
-        r_idx=r_m[l_cnt]
-        if i<r_idx and (R_s[r_idx]-R_s[i])>0:
-            res=max(res,(R_s[r_idx]-R_s[i])+l_cnt*2)
-
+        l_c.append(cnt)
+cnt=0
+for cur in s[::-1]:
+    if cur=='K':
+        cnt+=1
+    else:
+        r_c.append(cnt)
+r_c.reverse()
+left=0
+right=len(r_c)-1
+while left<=right:
+    res=max(res,right-left+1+min(l_c[left],r_c[right])*2)
+    if l_c[left]>r_c[right]:
+        right-=1
+    else:
+        left+=1
 print(res)
