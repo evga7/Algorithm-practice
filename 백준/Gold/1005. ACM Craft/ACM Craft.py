a5ff1ab8c1aebb21fad90a147f39a86a62362857
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-08-23 17:18:47 +0900

    [Gold III] Title: ACM Craft, Time: 468 ms, Memory: 131272 KB -BaekjoonHub

--- 백준/Gold/1005. ACM Craft/ACM Craft.py
+++ 백준/Gold/1005. ACM Craft/ACM Craft.py
@@ -1,31 +1,42 @@
+import heapq
+def is_inside(x, y, N, M):
+    return 0 <= x < N and 0 <= y < M
+import copy
 from collections import *
+import bisect
 import sys
-def input():return sys.stdin.readline().rstrip()
+#sys.setrecursionlimit(100000)
+def input(): return sys.stdin.readline().rstrip()
+MAX = sys.maxsize
+MOD = int(1e9) + 9
 T=int(input())
 def go():
-    q=deque()
+    q=[]
     for i in range(1,N+1):
-        if not de[i]:
-            q.append(i)
+        if not p[i]:
+            q.append((a[i-1],i))
             dist[i]=a[i-1]
     while q:
-        x=q.popleft()
-        for nxt in g[x]:
-            de[nxt]-=1
-            if not de[nxt]:
-                 q.append(nxt)
-            dist[nxt]=max(dist[nxt],dist[x]+a[nxt-1])
-    return dist[W]
+        cost,cur=heapq.heappop(q)
+        if dist[cur]<cost:continue
+        for nxt in g[cur]:
+            p[nxt]-=1
+            n_cost=cost+a[nxt-1]
+            if not p[nxt] and dist[nxt]>n_cost:
+                heapq.heappush(q,(n_cost,nxt))
+                dist[nxt]=n_cost
+
 while T:
     T-=1
-    N,K=map(int,input().split())
-    de = [0 for _ in range(N + 1)]
-    g=[[] for _ in range(N+1)]
+    g=defaultdict(list)
+    N,M=map(int,input().split())
+    p=[0 for _ in range(N+1)]
     a=list(map(int,input().split()))
-    dist=[0 for _ in range(N+1)]
-    for i in range(K):
-        c,d=map(int,input().split())
-        de[d]+=1
-        g[c].append(d)
+    for i in range(M):
+        q,w=map(int,input().split())
+        g[q].append(w)
+        p[w]+=1
     W=int(input())
-    print(go())
+    dist=[987654321 for _ in range(N+1)]
+    go()
+    print(dist[W])
