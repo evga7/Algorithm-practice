d45964653e6026ed630ab09aaa9d34558d0cce44
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-08-24 14:43:23 +0900

    [Gold III] Title: 사회망 서비스(SNS), Time: 4768 ms, Memory: 298216 KB -BaekjoonHub

--- 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.py
+++ 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.py
@@ -1,40 +1,28 @@
 from collections import *
 import bisect
 import sys
-sys.setrecursionlimit(10**8)
+sys.setrecursionlimit(10**7)
 def input(): return sys.stdin.readline().rstrip()
 MAX = sys.maxsize
 MOD = int(1e9) + 9
 N=int(input())
 g=defaultdict(list)
 g2=defaultdict(list)
-dp=[[-1 for _ in range(N+1)] for _ in range(2)]
+dp=[[0 for _ in range(N+1)] for _ in range(2)]
 visited=[0 for _ in range(N+1)]
 for i in range(N-1):
     q,w=map(int,input().split())
     g[q].append(w)
     g[w].append(q)
 
-def go(op,cur):
-    if dp[op][cur]!=-1:
-        return dp[op][cur]
-    ret=987654321
-    cnt=0
-    if op:
-        for nxt in g2[cur]:
-            cnt+=go(0,nxt)
-        ret=min(ret,cnt)
-    cnt=1
-    for nxt in g2[cur]:
-        cnt+=go(1,nxt)
-    ret = min(ret, cnt)
-    dp[op][cur] = ret
-    return dp[op][cur]
-def go2(cur):
+def go(cur):
     visited[cur]=1
+    dp[0][cur]=1
     for nxt in g[cur]:
         if visited[nxt]:continue
-        g2[cur].append(nxt)
-        go2(nxt)
-go2(1)
-print(min(go(0,1),go(1,1)))
+        go(nxt)
+        dp[1][cur]+=dp[0][nxt]
+        dp[0][cur]+=min(dp[0][nxt],dp[1][nxt])
+
+go(1)
+print(min(dp[0][1],dp[1][1]))
