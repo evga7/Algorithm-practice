64367369686b04db505e4e6191c35c96177e8527
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-08-25 21:44:43 +0900

    [Gold V] Title: 적록색약, Time: 176 ms, Memory: 115192 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -1,45 +1,45 @@
-dx = [1, -1, 0, 0]  # 아 위 오 왼
-dy = [0, 0, 1, -1]
+dx = [0,1,0,-1]  # 오 아 왼 위
+dy = [1,0,-1,0]
+dd=[0,4,2,4,4,1]
 import heapq
 def is_inside(x, y, N, M):
     return 0 <= x < N and 0 <= y < M
 import copy
 from collections import *
+import bisect
 import sys
-# sys.setrecursionlimit(100000)
+#sys.setrecursionlimit(100000)
 def input(): return sys.stdin.readline().rstrip()
+MAX = sys.maxsize
+MOD = int(1e9) + 9
 N=int(input())
-a=[list(input()) for _ in range(N)]
-visited=[[0 for _ in range(N+1)] for _ in range(N+1)]
-m=defaultdict(int)
-m['R']=1
-m['G']=2
-m['B']=3
-def go(sx,sy):
+v=[[0 for _ in range(N+1)] for _ in range(N+1)]
+res1,res2=0,0
+a=[list(input())for _ in range(N)]
+def go(sx,sy,op):
     q=deque()
     q.append((sx,sy))
-    visited[sx][sy]=1
     while q:
         x,y=q.popleft()
         for i in range(4):
             n_x=x+dx[i]
             n_y=y+dy[i]
-            if is_inside(n_x,n_y,N,N) and not visited[n_x][n_y] and m[a[sx][sy]]==m[a[n_x][n_y]]:
-                visited[n_x][n_y]=1
+            if is_inside(n_x,n_y,N,N) and a[n_x][n_y]==op and not v[n_x][n_y]:
+                v[n_x][n_y]=1
                 q.append((n_x,n_y))
-
-res1=0
-res2=0
 for i in range(N):
     for j in range(N):
-        if not visited[i][j]:
-            go(i,j)
+        if not v[i][j]:
+            go(i,j,a[i][j])
             res1+=1
-m['G']=1
-visited=[[0 for _ in range(N+1)] for _ in range(N+1)]
 for i in range(N):
     for j in range(N):
-        if not visited[i][j]:
-            go(i,j)
+        if a[i][j]=='G':
+            a[i][j]='R'
+v=[[0 for _ in range(N+1)] for _ in range(N+1)]
+for i in range(N):
+    for j in range(N):
+        if not v[i][j]:
+            go(i,j,a[i][j])
             res2+=1
 print(res1,res2)
