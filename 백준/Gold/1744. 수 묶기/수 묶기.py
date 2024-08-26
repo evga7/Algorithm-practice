f14843e672025f42031ca2fe0331da86d024dd0d
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-08-26 16:55:56 +0900

    [Gold IV] Title: 수 묶기, Time: 80 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Gold/1744. 수 묶기/수 묶기.py
+++ 백준/Gold/1744. 수 묶기/수 묶기.py
@@ -1,16 +1,18 @@
 import sys
-# sys.setrecursionlimit(100000)
+#sys.setrecursionlimit(100000)
 def input(): return sys.stdin.readline().rstrip()
+MAX = sys.maxsize
+MOD = int(1e9) + 9
 N=int(input())
-a=[int(input()) for _ in range(N)]
-dp=[-1001 for _ in range(N+1)]
+a=[int(input())for _ in range(N)]
+dp=[-MAX for _ in range(N+1)]
 a.sort()
 def go(idx):
     if idx==N:
         return 0
-    if dp[idx]!=-1001:
+    if dp[idx]!=-MAX:
         return dp[idx]
-    ret=-1001
+    ret=-MAX
     ret=max(ret,go(idx+1)+a[idx])
     if idx+1<N:
         ret=max(ret,go(idx+2)+(a[idx]*a[idx+1]))
