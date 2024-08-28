6b477c8fecb06a943346835cc10e7d61fcaba8d4
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-08-28 17:24:42 +0900

    [Gold II] Title: 나누기, Time: 712 ms, Memory: 417304 KB, Score: 69 point -BaekjoonHub

--- 백준/Gold/21757. 나누기/나누기.py
+++ 백준/Gold/21757. 나누기/나누기.py
@@ -1,31 +1,30 @@
-from collections import *
-import bisect
 import itertools
 import sys
-#sys.setrecursionlimit(100000)
+sys.setrecursionlimit(100000)
 def input(): return sys.stdin.readline().rstrip()
 MAX = sys.maxsize
 MOD = int(1e9) + 9
 N=int(input())
 a=list(map(int,input().split()))
-b=[0]+list(itertools.accumulate(a))
-dp=[[defaultdict(int) for _ in range(N+2)] for _ in range(5)]
-def go(cnt,idx,num):
-    if cnt>4:
-        return 0
-    if idx==N+1:
-        if cnt==3:
+b=list(itertools.accumulate(a))
+dp=[[-1 for _ in range(N)] for _ in range(4)]
+if b[-1]%4:
+    print(0)
+    exit(0)
+K=b[-1]//4
+def go(cnt,idx):
+    if cnt==3:
+        if idx<=N-1:
             return 1
         return 0
-    if num in dp[cnt][idx]:
-        return dp[cnt][idx][num]
+    if idx==N:
+        return 0
+    if dp[cnt][idx]!=-1:
+        return dp[cnt][idx]
     ret=0
-    for i in range(idx,N+1):
-        if b[i]-b[idx-1]==num:
-            ret+=go(cnt+1,i+1,num)
-    dp[cnt][idx][num]=ret
-    return dp[cnt][idx][num]
-res=0
-for i in range(1,N-2):
-    res+=go(0,i+1,b[i])
-print(res)
+    if b[idx]==(cnt+1)*K:
+        ret+=go(cnt+1,idx+1)
+    ret+=go(cnt,idx+1)
+    dp[cnt][idx]=ret
+    return dp[cnt][idx]
+print(go(0,0))
