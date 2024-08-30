a066bc96ef63b5d1452c2c13fc0bc08ee4613af8
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-08-30 12:13:44 +0900

    [level 0] Title: 정수를 나선형으로 배치하기, Time: 0.33 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.py
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.py
@@ -2,19 +2,19 @@ dx=[0,1,0,-1]
 dy=[1,0,-1,0]
 def solution(n):
     answer = [[0 for _ in range(n)] for _ in range(n)]
+    dir=0
+    num=1
     x,y=0,0
-    cnt=1
-    d=0
     while True:
-        answer[x][y]=cnt
-        cnt+=1
-        if cnt>n*n:
-            break
-        n_x,n_y=x+dx[d],y+dy[d]
-        while not (0<=n_x<n and 0<=n_y<n) or answer[n_x][n_y]:
-            d=(d+1)%4
-            n_x=x+dx[d]
-            n_y=y+dy[d]
+        answer[x][y]=num
+        if num==n*n:break
+        while True:
+            n_x=x+dx[dir]
+            n_y=y+dy[dir]
+            if 0<=n_x<n and 0<=n_y<n and not answer[n_x][n_y]:
+                break
+            dir+=1
+            dir%=4
         x,y=n_x,n_y
+        num+=1
     return answer
-print(solution(4))
