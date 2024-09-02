aebf46d78e62ea7b9ac6f69c03f1c6b9b34a9cfa
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-09-02 18:52:54 +0900

    [Silver II] Title: 부분수열의 합, Time: 144 ms, Memory: 241492 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,16 +1,27 @@
+import itertools
+import math
+from collections import *
 import sys
 sys.setrecursionlimit(100000)
 def input(): return sys.stdin.readline().rstrip()
+MAX = sys.maxsize
+MOD = int(1e9)
+def is_inside(x,y,N,M):
+    return 0<=x<N and 0<=y<M
+dx=[0,1,0,-1,-1,-1,1,1]
+dy=[1,0,-1,0,-1,1,1,-1]
 N,M=map(int,input().split())
 a=list(map(int,input().split()))
 res=0
-def go(idx,s,cnt):
+def go(idx,s):
     if idx==N:
-        global res
-        if s==M and cnt:
+        if s==M:
+            global res
             res+=1
         return
-    go(idx+1,s+a[idx],cnt+1)
-    go(idx+1,s,cnt)
-go(0,0,0)
+    go(idx+1,s)
+    go(idx + 1, s+a[idx])
+go(0,0)
+if M==0:
+    res-=1
 print(res)
