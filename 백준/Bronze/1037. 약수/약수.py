4a1e8a0326315d0163a5674ff55871bf9a9c39ac
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-09-02 16:27:42 +0900

    [Bronze I] Title: 약수, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.py
@@ -0,0 +1,10 @@
+import sys
+#sys.setrecursionlimit(100000)
+def input(): return sys.stdin.readline().rstrip()
+MAX = sys.maxsize
+MOD = int(1e9) + 9
+N=int(input())
+a=list(map(int,input().split()))
+a.sort()
+m,mx=min(a),max(a)
+print(m*mx)
