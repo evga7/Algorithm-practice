f68f9935879d6397a2f1cbb8ab9f0343275776cd
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-09-03 15:48:52 +0900

    [Gold III] Title: 순회강연, Time: 172 ms, Memory: 242604 KB -BaekjoonHub

--- 백준/Gold/2109. 순회강연/순회강연.py
+++ 백준/Gold/2109. 순회강연/순회강연.py
@@ -1,26 +1,28 @@
 import heapq
-def is_inside(x, y, N, M):
-    return 0 <= x < N and 0 <= y < M
-import copy
+import itertools
+import math
 from collections import *
 import sys
-# sys.setrecursionlimit(100000)
+sys.setrecursionlimit(100000)
 def input(): return sys.stdin.readline().rstrip()
+MAX = sys.maxsize
+MOD = int(1e9)
+def is_inside(x,y,N,M):
+    return 0<=x<N and 0<=y<M
+dx=[0,1,0,-1,-1,-1,1,1]
+dy=[1,0,-1,0,-1,1,1,-1]
 N=int(input())
+q=[]
 a=[list(map(int,input().split())) for _ in range(N)]
 a.sort(key=lambda x:x[1])
-res=0
-q=[]
-idx=0
-for cur in a:
-    cost,day=cur[0],cur[1]
+for cost,day in a:
     if len(q)<day:
         heapq.heappush(q,cost)
     else:
         if q[0]<cost:
             heapq.heappop(q)
             heapq.heappush(q,cost)
+res=0
 while q:
     res+=q.pop()
-
 print(res)
