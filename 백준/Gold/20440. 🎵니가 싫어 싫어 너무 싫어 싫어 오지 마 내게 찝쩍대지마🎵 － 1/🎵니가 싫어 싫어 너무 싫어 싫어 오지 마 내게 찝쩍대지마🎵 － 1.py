14465302551ae2aeea79f52440e2d38eb56127c9
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-09-14 02:58:35 +0900

    [Gold III] Title: 🎵니가 싫어 싫어 너무 싫어 싫어 오지 마 내게 찝쩍대지마🎵 - 1, Time: 868 ms, Memory: 422596 KB -BaekjoonHub

--- 백준/Gold/20440. 🎵니가 싫어 싫어 너무 싫어 싫어 오지 마 내게 찝쩍대지마🎵 － 1/🎵니가 싫어 싫어 너무 싫어 싫어 오지 마 내게 찝쩍대지마🎵 － 1.py
+++ 백준/Gold/20440. 🎵니가 싫어 싫어 너무 싫어 싫어 오지 마 내게 찝쩍대지마🎵 － 1/🎵니가 싫어 싫어 너무 싫어 싫어 오지 마 내게 찝쩍대지마🎵 － 1.py
@@ -1,44 +1,28 @@
-# N,M=map(int,input().split())
-# visited=[[987654321]*M for _ in range(N)]
-# arr=[list(map(int,input().strip())) for _ in range(N)]
-# str=re.findall(r'\d+',str) 숫자만 때기
-# re.findall(r'-?\d+', str1)] 음수는 이렇게
-# word1 = re.findall("[a-zA-Z]+", st) 이건 문자 추출
-#arr2 = [k[::-1] for k in zip(*arr)] 문자열 90도 회전
-import bisect
-import math
-import re
-import heapq
-import sys
 from collections import *
 import itertools
-import collections
-#sys.setrecursionlimit(10 ** 5)
-dx = [0,-1,0,1]  # 왼 위 오 아래
-dy = [-1,0,1,0]
-def input():return sys.stdin.readline().rstrip()
+import sys
+sys.setrecursionlimit(100000)
+def input(): return sys.stdin.readline().rstrip()
+MAX = sys.maxsize
+MOD = int(1e9) + 9
 N=int(input())
-arr=[list(map(int,input().split())) for _ in range(N)]
-arr.sort()
 m=defaultdict(int)
-for cur in arr:
-    s=cur[0]
-    e=cur[1]
+for i in range(N):
+    s,e=map(int,input().split())
     m[s]+=1
     m[e]-=1
 cnt=0
 res=0
-res1=0
-res2=0
-f=0
-for cur in sorted(m.keys()):
+res_s,res_e=0,0
+for cur in sorted(list(m.keys())):
     cnt+=m[cur]
-    if cnt>res:
+    if res<cnt:
         res=cnt
-        res1=cur
-        f=1
-    elif cnt<res and cnt-m[cur]==res and f:
-        res2=cur
-        f=0
+        res_s=cur
+        res_e=0
+    else:
+        c=m[cur]
+        if not res_e and c and cnt-m[cur]==res:
+            res_e=cur
 print(res)
-print(res1,res2)
+print(res_s,res_e)
