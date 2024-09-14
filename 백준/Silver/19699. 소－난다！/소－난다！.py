acabe66f5b290a2e28c329b40ec35a686f25c814
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-09-14 16:41:21 +0900

    [Silver II] Title: 소-난다!, Time: 112 ms, Memory: 240008 KB -BaekjoonHub

--- 백준/Silver/19699. 소－난다！/소－난다！.py
+++ 백준/Silver/19699. 소－난다！/소－난다！.py
@@ -1,49 +1,36 @@
-# N,M=map(int,input().split())
-# visited=[[987654321]*M for _ in range(N)]
-# arr=[list(map(int,input().strip())) for _ in range(N)]
-# str=re.findall(r'\d+',str) 숫자만 때기
-# re.findall(r'-?\d+', str1)] 음수는 이렇게
-# word1 = re.findall("[a-zA-Z]+", st) 이건 문자 추출
-#arr2 = [k[::-1] for k in zip(*arr)] 문자열 90도 회전
-import bisect
 import math
-import re
-
-import itertools
-import collections
-#sys.setrecursionlimit(10 ** 6)
 from collections import *
-from functools import cmp_to_key
-#dx = [0,-1,0,1]  # 오 위 왼 아
-#dy = [1,0,-1,0]
-dx=[1,1,1,0,0,0,-1,-1,-1]
-dy=[-1,0,1,-1,0,1,-1,0,1]
-import heapq
+import sys
+sys.setrecursionlimit(100000)
+def input(): return sys.stdin.readline().rstrip()
+MAX = sys.maxsize
+MOD = int(1e9)
 def is_inside(x,y,N,M):
     return 0<=x<N and 0<=y<M
-import sys
-def input():return sys.stdin.readline().rstrip()
-prime=[0 for _ in range(10001)]
-INF=sys.maxsize
+dx=[0,1,0,-1,-1,-1,1,1]
+dy=[1,0,-1,0,-1,1,1,-1]
 N,M=map(int,input().split())
 a=list(map(int,input().split()))
-p=itertools.combinations(a,M)
-def e():
-    prime[1]=1
-    for i in range(2,int(math.sqrt(10000))+1):
-        if prime[i]:continue
-        for j in range(i*i,10001,i):
-            prime[j]=1
-e()
-res_v=[]
 st=set()
-for cur in p:
-    s=sum(cur)
-    if not prime[s] and not s in st:
-        st.add(s)
-        res_v.append(s)
-if res_v:
-    res_v.sort()
-    print(*res_v)
-else:
+def chk(num):
+    if num==1:
+        return False
+    for i in range(2,int(math.sqrt(num))+1):
+        if not num%i:
+            return False
+    return True
+def go(idx,cnt,s):
+    if cnt==M:
+        if chk(s):
+            st.add(s)
+        return
+    if idx==N:
+        return
+    go(idx+1,cnt,s)
+    go(idx+1,cnt+1,s+a[idx])
+go(0,0,0)
+res=sorted(list(st))
+if not res:
     print(-1)
+else:
+    print(*res)
