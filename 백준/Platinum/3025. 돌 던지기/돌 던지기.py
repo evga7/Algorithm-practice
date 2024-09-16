32c5b976477f5bc713e9feaf8145f216fdebba71
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-09-16 14:29:15 +0900

    [Platinum IV] Title: 돌 던지기, Time: 440 ms, Memory: 327932 KB -BaekjoonHub

--- 백준/Platinum/3025. 돌 던지기/돌 던지기.py
+++ 백준/Platinum/3025. 돌 던지기/돌 던지기.py
@@ -1,41 +1,62 @@
+import heapq
+import itertools
+import math
+from collections import *
 import sys
-#sys.setrecursionlimit(200000)
-def input():return sys.stdin.readline().rstrip()
-INF=sys.maxsize
+sys.setrecursionlimit(100000)
+def input(): return sys.stdin.readline().rstrip()
+MAX = sys.maxsize
+MOD = int(1e9)
+def is_inside(x,y,N,M):
+    return 0<=x<N and 0<=y<M
+#dx=[0,1,0,-1,-1,-1,1,1]
+#dy=[1,0,-1,0,-1,1,1,-1]
+dx=[0,0,1,-1]
+dy=[1,-1,0,0]
 N,M=map(int,input().split())
 a=[list(input()) for _ in range(N)]
-chk=[[] for _ in range(M)]
 T=int(input())
-def go(x,y,sy):
-    while x+1<N:
-        chk[sy].append((x,y))
-        if x+1<N and a[x+1][y]=='.':
-            x+=1
-        elif x+1<N and a[x+1][y]=='X':
-            break
-        elif x+1<N and a[x+1][y]=='O':
-            if x+1<N and y-1>=0 and a[x][y-1]=='.' and a[x+1][y-1]=='.':
-                x+=1
-                y-=1
-            elif x+1<N and y+1<M and a[x][y+1]=='.' and a[x+1][y+1]=='.':
-                x+=1
-                y+=1
-            else:
-                break
-    if a[x][y]=='.':
-        a[x][y]='O'
-for i in range(T):
-    x=0
-    sy=int(input())-1
+st=[[] for _ in range(M+1)]
+def go(sx,sy,s_pos):
+    x=sx
     y=sy
-    while chk[sy]:
-        x,y=chk[sy][-1]
-        if a[x][y]=='.':
+    if a[x][y]=='X':return
+    while is_inside(x,y,N,M):
+        n_x=x+1
+        n_y=y
+        f=0
+        st[s_pos].append((x, y))
+        if is_inside(n_x,n_y,N,M):
+            if a[n_x][n_y]=='X':
+                break
+            elif a[n_x][n_y]=='O':
+                if is_inside(x,y-1,N,M) and is_inside(n_x,y-1,N,M) and a[x][y-1]=='.' and a[n_x][y-1]=='.':
+                    f=1
+                    x,y=n_x,y-1
+                elif is_inside(x,y+1,N,M) and is_inside(n_x,y+1,N,M) and a[x][y+1]=='.' and a[n_x][y+1]=='.':
+                    f=1
+                    x,y=n_x,y+1
+                else:
+                    break
+        else:
             break
-        chk[sy].pop()
-    go(x,y,sy)
+        if not f:
+            x,y=n_x,n_y
+    a[x][y]='O'
+for i in range(T):
+    pos=int(input())
+    if st[pos-1]:
+        sx,sy=st[pos-1].pop()
+    else:
+        sx,sy=0,pos-1
+    while st[pos-1] and a[sx][sy]=='O':
+        sx, sy = st[pos - 1].pop()
+    if a[sx][sy]!='O':
+        go(sx,sy,pos-1)
+
 for i in range(N):
     for j in range(M):
         print(a[i][j],end='')
     print('')
-    
+
+
