bdccc3ed3587e3dbbe748653d37490df66084373
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-09-18 10:23:06 +0900

    [Silver IV] Title: 상근이의 여행, Time: 124 ms, Memory: 237060 KB -BaekjoonHub

--- 백준/Silver/9372. 상근이의 여행/상근이의 여행.py
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.py
@@ -1,11 +1,18 @@
 import sys
+sys.setrecursionlimit(100000)
 def input(): return sys.stdin.readline().rstrip()
 MAX = sys.maxsize
-MOD = int(1e9) + 9
-T = int(input())
+MOD = int(1e9)
+def is_inside(x,y,N,M):
+    return 0<=x<N and 0<=y<M
+#dx=[0,1,0,-1,-1,-1,1,1]
+#dy=[1,0,-1,0,-1,1,1,-1]
+dx=[0,0,1,-1]
+dy=[1,-1,0,0]
+T=int(input())
 while T:
-    T -= 1
-    N, M = map(int, input().split())
+    T-=1
+    N,M=map(int,input().split())
     for i in range(M):
-        a, b = map(int, input().split())
+        a,b = map(int, input().split())
     print(N-1)
