7e7cd69af03ad8eb286221636cf0ed57acca9464
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-09-25 14:14:16 +0900

    [Bronze III] Title: 완전 세제곱, Time: 140 ms, Memory: 240740 KB -BaekjoonHub

--- 백준/Bronze/4690. 완전 세제곱/완전 세제곱.py
+++ 백준/Bronze/4690. 완전 세제곱/완전 세제곱.py
@@ -1,10 +1,20 @@
+from collections import *
 import sys
-#sys.setrecursionlimit(100000)
-def input():return sys.stdin.readline().rstrip()
-INF=sys.maxsize
-for num in range(6,101):
-    for i in range(2,101):
-        for j in range(i+1,101):
-            for k in range(j+1,101):
-                if (pow(i,3)+pow(j,3)+pow(k,3))==pow(num,3):
-                    print("Cube = %d, Triple = (%d,%d,%d)"% (num,i,j,k))
+sys.setrecursionlimit(100000)
+def input(): return sys.stdin.readline().rstrip()
+MAX = sys.maxsize
+MOD = int(1e9)
+def is_inside(x,y,N,M):
+    return 0<=x<N and 0<=y<M
+#dx=[0,1,0,-1,-1,-1,1,1]
+#dy=[1,0,-1,0,-1,1,1,-1]
+dx=[0,0,1,-1]
+dy=[1,-1,0,0]
+g=defaultdict(int)
+for i in range(3,101):
+    for j in range(2,i):
+        for k in range(j+1,i):
+            for l in range(k+1,i):
+                a,b,c=pow(j,3),pow(k,3),pow(l,3)
+                if pow(i,3)==(a+b+c):
+                    print("Cube = %d, Triple = (%d,%d,%d)"%(i,j,k,l))
