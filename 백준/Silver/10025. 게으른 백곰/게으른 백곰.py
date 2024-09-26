391a84bbf43ba6099c646c7f94282e7e846a0b5a
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-09-26 18:17:50 +0900

    [Silver III] Title: 게으른 백곰, Time: 220 ms, Memory: 249656 KB -BaekjoonHub

--- 백준/Silver/10025. 게으른 백곰/게으른 백곰.py
+++ 백준/Silver/10025. 게으른 백곰/게으른 백곰.py
@@ -1,28 +1,27 @@
-from collections import *
 import sys
-#sys.setrecursionlimit(200000)
-def input():return sys.stdin.readline().rstrip()
-INF=sys.maxsize
-
+sys.setrecursionlimit(100000)
+def input(): return sys.stdin.readline().rstrip()
+MAX = sys.maxsize
+MOD = int(1e9)
 def is_inside(x, y, N, M):
     return 0 <= x < N and 0 <= y < M
+# dx=[0,1,0,-1,-1,-1,1,1]
+# dy=[1,0,-1,0,-1,1,1,-1]
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
 N,M=map(int,input().split())
-m=defaultdict(int)
-for _ in range(N):
-    b,c=map(int,input().split())
-    m[c]=b
-
-cnt=0
-for i in range(M*2+1):
-    if m[i]:
-        cnt+=m[i]
-res=cnt
+a=[list(map(int,input().split())) for _ in range(N)]
+a.sort(key=lambda x:x[1])
 left=0
-right=M*2
-while right<=2000000:
-    cnt-=m[left]
-    left+=1
-    right+=1
-    cnt+=m[right]
-    res=max(res,cnt)
+right=0
+s=0
+res=0
+while left<=right and right<N:
+    if a[right][1]-a[left][1]<=M*2:
+        s+=a[right][0]
+        res=max(res,s)
+        right+=1
+    else:
+        s-=a[left][0]
+        left+=1
 print(res)
