a385008ef668439a4b4d584dc791788ef52e8747
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-09-29 17:30:45 +0900

    [Bronze III] Title: 수학적 호기심, Time: 384 ms, Memory: 238128 KB -BaekjoonHub

--- 백준/Bronze/9094. 수학적 호기심/수학적 호기심.py
+++ 백준/Bronze/9094. 수학적 호기심/수학적 호기심.py
@@ -1,20 +1,20 @@
 import sys
-#sys.setrecursionlimit(200000)
-def input():return sys.stdin.readline().rstrip()
-INF=sys.maxsize
-
+sys.setrecursionlimit(100000)
+def input(): return sys.stdin.readline().rstrip()
+MAX = sys.maxsize
+MOD = int(1e9)
 def is_inside(x, y, N, M):
     return 0 <= x < N and 0 <= y < M
+# dx=[0,1,0,-1,-1,-1,1,1]
+# dy=[1,0,-1,0,-1,1,1,-1]
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
 T=int(input())
-
-for _ in range(T):
+for i in range(T):
     N,M=map(int,input().split())
     res=0
     for i in range(1,N):
         for j in range(i+1,N):
-            div=i*j
-            a=(pow(i, 2) + pow(j, 2) + M) // div
-            b = (pow(i, 2) + pow(j, 2) + M) / div
-            if a==b:
+            if ((pow(i,2)+pow(j,2)+M)/(i*j))==((pow(i,2)+pow(j,2)+M)//(i*j)):
                 res+=1
     print(res)
