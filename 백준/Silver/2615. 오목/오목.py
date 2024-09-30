d6470884f5a994d38b5a6aecb60099fca644ad03
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-09-30 19:46:45 +0900

    [Silver I] Title: 오목, Time: 120 ms, Memory: 238468 KB -BaekjoonHub

--- 백준/Silver/2615. 오목/오목.py
+++ 백준/Silver/2615. 오목/오목.py
@@ -1,40 +1,46 @@
+import heapq
+import math
+from collections import *
+import sys
+sys.setrecursionlimit(100000)
+def input(): return sys.stdin.readline().rstrip()
+MAX = sys.maxsize
+MOD = int(1e9)
 def is_inside(x, y, N, M):
     return 0 <= x < N and 0 <= y < M
-import sys
-dx=[0,1,1,-1]
-dy=[1,1,0,1]
-def input(): return sys.stdin.readline().rstrip()
+# dx=[0,1,0,-1,-1,-1,1,1]
+# dy=[1,0,-1,0,-1,1,1,-1]
+#dx = [0, 0, 1, -1]
+#dy = [1, -1, 0, 0]
+dx = [0, 1,1,1]
+dy = [1, 0,1,-1]
 a=[list(map(int,input().split())) for _ in range(19)]
-v=[[[0 for _ in range(8)] for _ in range(19)] for _ in range(19)]
-def go(x,y):
-    val=a[x][y]
+c=[[[0 for _ in range(19)] for _ in range(19)] for _ in range(4)]
+def chk(x,y,op):
     for i in range(4):
-        cnt=0
-        n_x=x-dx[i]
-        n_y=y-dy[i]
-        if is_inside(n_x,n_y,19,19) and a[n_x][n_y]==val:
-            continue
-        for j in range(6):
+        cnt=1
+        n_x=x
+        n_y=y
+        v=[]
+        v.append((y,x))
+        for j in range(19):
+            if c[i][n_x][n_y]:break
+            c[i][n_x][n_y]=1
             n_x+=dx[i]
             n_y+=dy[i]
-            if is_inside(n_x,n_y,19,19) and val==a[n_x][n_y]:
+            if not is_inside(n_x,n_y,19,19):break
+            if a[n_x][n_y]==op:
                 cnt+=1
+                v.append((n_y, n_x))
             else:
-                if cnt==5:
-                    return (x, y, val)
-                cnt=0
+                break
         if cnt==5:
-            return (x,y,val)
-    return (-1,-1,-1)
-
-
+            v.sort()
+            print(op)
+            print(v[0][1]+1,v[0][0]+1)
+            exit(0)
 for i in range(19):
     for j in range(19):
         if a[i][j]:
-            res=go(i,j)
-            if res[0]!=-1:
-                print(res[2])
-                print(res[0]+1,res[1]+1)
-                exit(0)
-
+            chk(i,j,a[i][j])
 print(0)
