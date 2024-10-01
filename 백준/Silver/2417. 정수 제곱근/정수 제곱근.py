facbc39a5e3e028e039671f1591b672da45f33cf
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-10-01 21:55:04 +0900

    [Silver IV] Title: 정수 제곱근, Time: 108 ms, Memory: 240008 KB -BaekjoonHub

--- 백준/Silver/2417. 정수 제곱근/정수 제곱근.py
+++ 백준/Silver/2417. 정수 제곱근/정수 제곱근.py
@@ -1,29 +1,21 @@
-# N,M=map(int,input().split())
-# visited=[[987654321]*M for _ in range(N)]
-# arr=[list(map(int,input().strip())) for _ in range(N)]
-# str=re.findall(r'\d+',str) 숫자만 때기
-# re.findall(r'-?\d+', str1)] 음수는 이렇게
-# word1 = re.findall("[a-zA-Z]+", st) 이건 문자 추출
-#arr2 = [k[::-1] for k in zip(*arr)] 문자열 90도 회전
-import bisect
 import math
-import re
-import heapq
-import sys
 from collections import *
-import itertools
-import collections
-si=sys.stdin.readline
-#sys.setrecursionlimit(10 ** 6)
-dx = [0,-1,0,1]  # 왼 위 오 아래
-dy = [-1,0,1,0]
-res=0
-r=0
-def input():return sys.stdin.readline().rstrip()
+import sys
+sys.setrecursionlimit(100000)
+def input(): return sys.stdin.readline().rstrip()
+MAX = sys.maxsize
+MOD = int(1e9)+7
+def is_inside(x,y,N,M):
+    return 0<=x<N and 0<=y<M
+#dx=[0,1,0,-1,-1,-1,1,1]
+#dy=[1,0,-1,0,-1,1,1,-1]
+#dx=[0,0,1,-1]
+#dy=[1,-1,0,0]
+dx=[1,1,-1,-1]
+dy=[1,-1,-1,1]
 N=int(input())
-n=int(math.sqrt(N))
-while n*n<N:
-    n+=1
-print(n)
-
-
+a=int(math.sqrt(N))
+if a*a>=N:
+    print(a)
+else:
+    print(a+1)
