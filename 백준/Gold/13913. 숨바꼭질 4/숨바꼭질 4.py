0d58cefb5be2e6043a50251459b38670261a31e3
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-10-03 14:20:53 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 156 ms, Memory: 254412 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.py
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.py
@@ -1,28 +1,35 @@
 import heapq
-def is_inside(x,y,N,M):
-    return 0<=x<N and 0<=y<M
+import math
+from collections import *
 import sys
-def input():return sys.stdin.readline().rstrip()
-INF=sys.maxsize
-st=[0 for _ in range(100001)]
-visited=set()
+sys.setrecursionlimit(100000)
+def input(): return sys.stdin.readline().rstrip()
+MAX = sys.maxsize
+MOD = int(1e9)
+def is_inside(x, y, N, M):
+    return 0 <= x < N and 0 <= y < M
+# dx=[0,1,0,-1,-1,-1,1,1]
+# dy=[1,0,-1,0,-1,1,1,-1]
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
 N,M=map(int,input().split())
-q=[]
+q=deque()
+m=defaultdict(int)
 q.append((0,N))
+
 while q:
-    cost,x=heapq.heappop(q)
-    if x==M:
+    cost,cur=q.popleft()
+    if cur==M:
         print(cost)
         break
-    for nxt in (x*2,x-1,x+1):
-        if 0<=nxt<=100000 and not nxt in visited:
-            visited.add(nxt)
-            heapq.heappush(q,(cost+1,nxt))
-            st[nxt]=x
-res_v=[]
+    for nxt in [cur*2,cur+1,cur-1]:
+        if 0<=nxt<=100000 and not nxt in m:
+            m[nxt]=cur
+            q.append((cost+1,nxt))
+v=[]
 while M!=N:
-    res_v.append(M)
-    M=st[M]
-res_v.append(N)
-res_v.reverse()
-print(*res_v)
+    v.append(M)
+    M=m[M]
+v.append(N)
+for i in range(len(v)-1,-1,-1):
+    print(v[i],end=' ')
