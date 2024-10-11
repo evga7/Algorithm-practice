a205fb90a88914c0f59d26d27168d168c90e7e58
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-10-11 12:39:15 +0900

    [Bronze II] Title: 문어, Time: 88 ms, Memory: 236084 KB -BaekjoonHub

--- 백준/Bronze/21313. 문어/문어.py
+++ 백준/Bronze/21313. 문어/문어.py
@@ -1,15 +1,16 @@
-
 import sys
-#sys.setrecursionlimit(200000)
-def input():return sys.stdin.readline().rstrip()
-INF=sys.maxsize
+sys.setrecursionlimit(100000)
+def input(): return sys.stdin.readline().rstrip()
+MAX = sys.maxsize
+MOD = int(1e9)+7
+def is_inside(x,y,N,M):
+    return 0<=x<N and 0<=y<M
+#dx=[0,1,0,-1,-1,-1,1,1]
+#dy=[1,0,-1,0,-1,1,1,-1]
+dx=[0,0,1,-1]
+dy=[1,-1,0,0]
 N=int(input())
-for i in range(N-1):
-    if i&1:
-        print(2,end= ' ')
-    else:
-        print(1,end=' ')
+res='1 2 '*(N//2)
 if N&1:
-    print(3)
-else:
-    print(2)
+    res+='3'
+print(res)
