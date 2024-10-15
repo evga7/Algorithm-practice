6fb4c9eeb0d1cfa5679bbfec0f592dd56f745403
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-10-15 22:17:56 +0900

    [Gold II] Title: 보석 도둑, Time: 1160 ms, Memory: 308920 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.py
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.py
@@ -1,24 +1,27 @@
-import heapq
-def is_inside(x,y,N,M):
-    return 0<=x<N and 0<=y<M
-import copy
-from collections import *
 import sys
-#sys.setrecursionlimit(100000)
+import heapq
+sys.setrecursionlimit(100000)
 def input(): return sys.stdin.readline().rstrip()
+MAX = sys.maxsize
+MOD = int(1e9)
+def is_inside(x, y, N, M):
+    return 0 <= x < N and 0 <= y < M
+# dx=[0,1,0,-1,-1,-1,1,1]
+# dy=[1,0,-1,0,-1,1,1,-1]
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
 N,K=map(int,input().split())
 a=[list(map(int,input().split())) for _ in range(N)]
 b=[int(input()) for _ in range(K)]
+a.sort(key=lambda x:(x[0],-x[1]))
 b.sort()
-a.sort()
-q=[]
 idx=0
+q=[]
 res=0
 for cur in b:
     while idx<N and cur>=a[idx][0]:
         heapq.heappush(q,-a[idx][1])
         idx+=1
     if q:
-        res+=heapq.heappop(q)
-    
-print(-res)
+        res-=heapq.heappop(q)
+print(res)
