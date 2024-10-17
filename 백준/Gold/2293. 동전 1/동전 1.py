b7b6dce2b156ee9ce4465e6a11568e829507899b
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-10-17 23:16:28 +0900

    [Gold IV] Title: 동전 1, Time: 112 ms, Memory: 111512 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.py
+++ 백준/Gold/2293. 동전 1/동전 1.py
@@ -1,11 +1,11 @@
 import sys
-def input():return sys.stdin.readline().rstrip()
+def input(): return sys.stdin.readline().rstrip()
 N,M=map(int,input().split())
 a=[int(input()) for _ in range(N)]
+a.sort()
 dp=[0 for _ in range(M+1)]
 dp[0]=1
-a.sort()
 for cur in a:
-    for j in range(cur,M+1):
-        dp[j]+=dp[j-cur]
+    for i in range(cur,M+1):
+        dp[i]+=dp[i-cur]
 print(dp[M])
