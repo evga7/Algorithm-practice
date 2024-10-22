42503356007f0d5194fffadd91f4dabd2aa07072
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-10-22 22:26:44 +0900

    [Gold V] Title: 알약, Time: 120 ms, Memory: 110424 KB -BaekjoonHub

--- 백준/Gold/4811. 알약/알약.py
+++ 백준/Gold/4811. 알약/알약.py
@@ -1,17 +1,31 @@
+import bisect
+import heapq
+import itertools
+import math
+from collections import *
 import sys
+#sys.setrecursionlimit(100000)
 def input(): return sys.stdin.readline().rstrip()
+MAX = sys.maxsize
+MOD = int(1e9)
+def is_inside(x, y, N, M):
+    return 0 <= x < N and 0 <= y < M
+# dx=[0,1,0,-1,-1,-1,1,1]
+# dy=[1,0,-1,0,-1,1,1,-1]
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
 dp=[[-1 for _ in range(31)] for _ in range(31)]
-def go(W,H):
-    if W==0:
+def go(N,half):
+    if N==0:
         return 1
-    if dp[W][H]!=-1:
-        return dp[W][H]
+    if dp[N][half]!=-1:
+        return dp[N][half]
     ret=0
-    ret+=go(W-1,H+1)
-    if H:
-        ret+=go(W,H-1)
-    dp[W][H]=ret
-    return dp[W][H]
+    ret=go(N-1,half+1)
+    if half:
+        ret+=go(N,half-1)
+    dp[N][half]=ret
+    return dp[N][half]
 while True:
     N=int(input())
     if not N:
