ff33714b52d0eb283abfb18028089ff7b7f1f03a
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-10-23 21:55:49 +0900

    [Gold IV] Title: N으로 만들기, Time: 112 ms, Memory: 112004 KB -BaekjoonHub

--- 백준/Gold/17255. N으로 만들기/N으로 만들기.py
+++ 백준/Gold/17255. N으로 만들기/N으로 만들기.py
@@ -1,29 +1,31 @@
 from collections import *
+import sys
 #sys.setrecursionlimit(100000)
-dx = [1, -1, 0, 0]  # 아 위 오 왼
-dy = [0, 0, 1, -1]
+def input(): return sys.stdin.readline().rstrip()
+MAX = sys.maxsize
+MOD = int(1e9)
 def is_inside(x, y, N, M):
     return 0 <= x < N and 0 <= y < M
-
-
-import sys
-def input():return sys.stdin.readline().rstrip()
-N=input()
-q=deque()
+# dx=[0,1,0,-1,-1,-1,1,1]
+# dy=[1,0,-1,0,-1,1,1,-1]
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
 st=set()
+s=input()
+q=deque()
+q.append((len(s),s,""))
 res=0
-q.append((N,N,len(N)))
 while q:
-    cur,t,l=q.popleft()
+    l,cur,t=q.popleft()
     if l==1:
         res+=1
         continue
-    left=cur[1:]
-    right=cur[:l-1]
+    left = cur[1:]
+    right=cur[:-1]
     if not t+left in st:
         st.add(t+left)
-        q.append((left,t+left,l-1))
-    if not t+right in st:
-        st.add(t+right)
-        q.append((right,t+right,l-1))
+        q.append((l-1,left,t+left))
+    if not t + right in st:
+        st.add(t + right)
+        q.append((l - 1, right, t + right))
 print(res)
