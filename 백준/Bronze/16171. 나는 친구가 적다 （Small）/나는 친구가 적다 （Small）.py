8ceab8c93906482edfb36a4200faf61fbf725bfd
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-11-08 23:26:40 +0900

    [Bronze II] Title: 나는 친구가 적다 (Small), Time: 92 ms, Memory: 236084 KB -BaekjoonHub

--- 백준/Bronze/16171. 나는 친구가 적다 （Small）/나는 친구가 적다 （Small）.py
+++ 백준/Bronze/16171. 나는 친구가 적다 （Small）/나는 친구가 적다 （Small）.py
@@ -1,13 +1,18 @@
 import sys
-def input():
-    return sys.stdin.readline().rstrip()
+sys.setrecursionlimit(100000)
+def input(): return sys.stdin.readline().rstrip()
+MAX = sys.maxsize
+MOD = int(1e9)
+def is_inside(x, y, N, M):
+    return 0 <= x < N and 0 <= y < M
+# dx=[0,1,0,-1,-1,-1,1,1]
+# dy=[1,0,-1,0,-1,1,1,-1]
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
 s=input()
 s2=input()
-s3=""
+t=""
 for cur in s:
     if cur.isalpha():
-        s3+=cur
-if s3.find(s2)>=0:
-    print(1)
-else:
-    print(0)
+        t+=cur
+print(1) if s2 in t else print(0)
