4bf08cc6c381d85f5904c56ef644b11c2a6413c5
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-11-23 00:57:24 +0900

    [Silver I] Title: 정수 삼각형, Time: 12 ms, Memory: 3980 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
@@ -1,30 +1,57 @@
 #include <bits/stdc++.h>
+#define MAX 987654321
+#define MIN -987654321
 using namespace std;
-int dy[2]={0,1};
+typedef long long int ll;
+typedef pair<int, int> pi;
+typedef pair<int, string> pi3;
+typedef pair<int, pair<int, int>>pi2;
+typedef pair<int, char> pi4;
+typedef vector<vector<int>> vpi;
+typedef vector<int> vec;
+int dx[4] = { 0,1,0,-1 };
+int dy[4] = { 1,0,-1,0 };
+int N, M;
+int num;
+int res;
+vec v;
+int m[501][501];
 int dp[501][501];
-int t[501][501];
-int N;
-int solve(int x,int y){
-    if (x==N-1){
-        return t[x][y];
-    }
-    int&ret=dp[x][y];
-    if (ret!=-1)return ret;
-    return ret=max(solve(x+1,y),solve(x+1,y+1))+t[x][y];
+int solve()
+{
+	int i, j;
+	dp[0][0] = m[0][0];
+	for (i = 1; i < N; i++)
+	{
+		for (j = 0; j <=i; j++)
+		{
+			if (j == 0)
+				dp[i][j] = dp[i - 1][j]+m[i][j];
+			else if (j == i)
+				dp[i][j] = dp[i - 1][j - 1] + m[i][j];
+			else
+				dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - 1]) + m[i][j];
+		}
+	}
+	for (i = 0; i <= N; i++)
+	{
+		res = max(res, dp[N - 1][i]);
+	}
+	return res;
 }
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 	cout.tie(0);
-    cin >> N;
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j <= i; j++)
-        {
-            cin >> t[i][j];
-        }
-    }
-    memset(dp, -1, sizeof(dp));
-    cout << solve(0, 0);
+	int i, j;
+	cin >> N;
+	for (i = 0; i < N; i++)
+	{
+		for (j = 0; j <= i; j++)
+		{
+			cin >> m[i][j];
+		}
+	}
+	cout<< solve();
 }
