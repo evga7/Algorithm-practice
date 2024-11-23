db40254903b8cd9b4ce2e3a74ab3a5e5d74f028e
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-11-23 14:07:27 +0900

    [Gold V] Title: Fly me to the Alpha Centauri, Time: 100 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.py
+++ 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.py
@@ -1,18 +1,38 @@
 import sys
-def min_moves(x, y):
-    distance = y - x
-    k = 1
+#sys.setrecursionlimit(100000)
+def input(): return sys.stdin.readline().rstrip()
+MAX = sys.maxsize
+MOD = int(1e9)
+def is_inside(x, y, N, M):
+    return 0 <= x < N and 0 <= y < M
+# dx=[0,1,0,-1,-1,-1,1,1]
+# dy=[1,0,-1,0,-1,1,1,-1]
+dx = [0, 0, 1,-1]
+dy = [1, -1, 0, 0]
 
-    while k * (k + 1) < distance:
+def min_steps_to_reach(x, y):
+    distance = y - x
+    if distance == 0:
+        return 0
+
+    # k 값을 증가시키며 최대한 목표에 도달할 수 있는 최소 횟수를 구함
+    k = 1
+    total_distance = 0
+    steps = 0
+
+    while total_distance < distance:
+        total_distance += k
+        steps += 1
+        if total_distance >= distance:
+            break
+        total_distance += k
+        steps += 1
         k += 1
 
-    if distance <= k * k:
-        return 2 * k - 1
-    else:
-        return 2 * k
+    return steps
 
-# 테스트
-N=int(input())
-for i in range(N):
+# 테스트 케이스 입력
+t = int(input())
+for _ in range(t):
     x, y = map(int, input().split())
-    print(min_moves(x, y))
+    print(min_steps_to_reach(x, y))
