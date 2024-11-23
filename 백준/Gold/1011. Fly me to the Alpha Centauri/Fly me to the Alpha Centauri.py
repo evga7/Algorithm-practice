ec489befc9843ffe2edff6eabbbb53b0982bd799
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-11-23 20:23:59 +0900

    [Gold V] Title: Fly me to the Alpha Centauri, Time: 96 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.py
+++ 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.py
@@ -9,30 +9,28 @@ def is_inside(x, y, N, M):
 # dy=[1,0,-1,0,-1,1,1,-1]
 dx = [0, 0, 1,-1]
 dy = [1, -1, 0, 0]
+T=int(input())
+def solve(N):
+    location = 0
+    middle = N // 2
+    count = 0
+    max_speed = 1
 
-def min_steps_to_reach(x, y):
-    distance = y - x
-    if distance == 0:
-        return 0
+    while location < middle:
+        location += max_speed
+        max_speed += 1
+        count += 1
 
-    # k 값을 증가시키며 최대한 목표에 도달할 수 있는 최소 횟수를 구함
-    k = 1
-    total_distance = 0
-    steps = 0
+    rest = N - location
+    speed = 1
+    while rest > 0:
+        rest -= speed
+        count += 1
+        if speed < max_speed - 1:
+            speed += 1
 
-    while total_distance < distance:
-        total_distance += k
-        steps += 1
-        if total_distance >= distance:
-            break
-        total_distance += k
-        steps += 1
-        k += 1
-
-    return steps
-
-# 테스트 케이스 입력
-t = int(input())
-for _ in range(t):
-    x, y = map(int, input().split())
-    print(min_steps_to_reach(x, y))
+    return count
+while T:
+    T-=1
+    a,b=map(int,input().split())
+    print(solve(b-a))
