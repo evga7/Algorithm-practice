23d915996a15f715628c2f42b021d96c4e267bba
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2024-12-01 16:41:58 +0900

    [Gold V] Title: 용액, Time: 140 ms, Memory: 128156 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.py
+++ 백준/Gold/2467. 용액/용액.py
@@ -1,20 +1,32 @@
+from collections import *
 import sys
-def input():return sys.stdin.readline().rstrip()
+#sys.setrecursionlimit(100000)
+def input(): return sys.stdin.readline().rstrip()
+MAX = sys.maxsize
+MOD = int(1e9)
+def is_inside(x, y, N, M):
+    return 0 <= x < N and 0 <= y < M
+# dx=[0,1,0,-1,-1,-1,1,1]
+# dy=[1,0,-1,0,-1,1,1,-1]
+#dx = [0, 0, 1, -1]
+#dy = [1, -1, 0, 0]
+dx=[1,1,1]
+dy=[-1,0,1]
 N=int(input())
 a=list(map(int,input().split()))
 left=0
 right=N-1
-r=int(1e10)
-res1=-1
-res2=-1
+res=int(1e10)
+res_left=0
+res_right=0
 while left<right:
     s=a[left]+a[right]
-    if abs(s) <=abs(r):
-        r = abs(s)
-        res1 = a[left]
-        res2 = a[right]
+    if abs(s)<res:
+        res=abs(s)
+        res_left=a[left]
+        res_right = a[right]
     if s>=0:
         right-=1
     else:
         left+=1
-print(res1,res2)
+print(res_left,res_right)
