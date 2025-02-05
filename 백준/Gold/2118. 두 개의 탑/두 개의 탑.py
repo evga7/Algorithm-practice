f5295c0115d0c15afe73904b5bc36e1094fbce41
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2025-02-05 14:15:21 +0900

    [Gold V] Title: 두 개의 탑, Time: 140 ms, Memory: 118568 KB -BaekjoonHub

--- 백준/Gold/2118. 두 개의 탑/두 개의 탑.py
+++ 백준/Gold/2118. 두 개의 탑/두 개의 탑.py
@@ -1,10 +1,9 @@
 import bisect
 import heapq
 import itertools
-import math
-from collections import *
 import sys
-sys.setrecursionlimit(100000)
+from collections import *
+#sys.setrecursionlimit(100000)
 def input(): return sys.stdin.readline().rstrip()
 MAX = sys.maxsize
 MOD = int(1e9)
@@ -12,10 +11,10 @@ def is_inside(x, y, N, M):
     return 0 <= x < N and 0 <= y < M
 # dx=[0,1,0,-1,-1,-1,1,1]
 # dy=[1,0,-1,0,-1,1,1,-1]
-dx = [0, 0, 1, -1]
+dx = [0, 0, 1,-1]
 dy = [1, -1, 0, 0]
 N=int(input())
-a=[int(input()) for _ in range(N)]
+a=list(int(input()) for _ in range(N))
 b=[0]+list(itertools.accumulate(a))
 S=b[N]
 res=0
@@ -32,4 +31,3 @@ for i in range(N):
             right=mid-1
         res=max(res,min(SS,SS2))
 print(res)
-
