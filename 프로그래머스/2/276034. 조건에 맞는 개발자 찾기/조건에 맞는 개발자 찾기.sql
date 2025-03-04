ccdf0c48c0d0162eb53691e3df0dfd44b0131367
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2025-03-05 00:56:09 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -1,5 +1,5 @@
 -- 코드를 작성해주세요
-select b.ID,b.EMAIL,b.FIRST_NAME,b.LAST_NAME from DEVELOPERS b
-where b.skill_code & (select code from skillcodes where name like '%Python%') or 
-b.skill_code & (select code from skillcodes where name like '%C#%')
-order by b.id
+select ID,EMAIL,FIRST_NAME,LAST_NAME from DEVELOPERS
+where SKILL_CODE & (select CODE from SKILLCODES where name = 'Python')
+or SKILL_CODE & (select CODE from SKILLCODES where name = 'C#')
+order by ID
