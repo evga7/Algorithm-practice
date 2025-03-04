4a9ed6ed13f736171c6f5df1b40280eafc4accce
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2025-03-04 17:02:57 +0900

    [level 2] Title: ROOT 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/273710. ROOT 아이템 구하기/ROOT 아이템 구하기.sql
+++ 프로그래머스/2/273710. ROOT 아이템 구하기/ROOT 아이템 구하기.sql
@@ -1,4 +1,5 @@
 -- 코드를 작성해주세요
-select a.item_id,a.item_name from item_info a join item_tree b on a.item_id=b.item_id
-where b.PARENT_ITEM_ID is NULL
-order by item_id
+select a.ITEM_ID,a.ITEM_NAME from ITEM_INFO a join ITEM_TREE b on a.ITEM_ID=b.ITEM_ID
+where b.PARENT_ITEM_ID is null
+group by a.item_id
+order by a.item_id
