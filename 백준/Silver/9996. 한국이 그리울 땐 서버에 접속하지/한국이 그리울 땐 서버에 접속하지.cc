a48c923cd15e067422887d5c89a862242087b985
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2025-03-08 23:05:09 +0900

    [Silver III] Title: 한국이 그리울 땐 서버에 접속하지, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cc
+++ 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cc
@@ -1,62 +1,71 @@
 #include <bits/stdc++.h>
 #define MAX 987654321
 #define MIN -987654321
+#define MOD 1000000009
+#define pb push_back
+#define pob pop_back
+#define ms memset
 using namespace std;
 typedef long long int ll;
-typedef pair<int, int> pii;
+typedef pair<int, int> pi;
 typedef pair<int, string> pi3;
 typedef pair<int, pair<int, int>>pi2;
-typedef vector<vector<int>> vvi;
-typedef vector<pii>vpi;
+typedef vector<vector<int>> vvec;
 typedef vector<int> vec;
-typedef vector<pii> vec2;
-typedef queue<pii> que;
-typedef vector<vector<pii>> gra;
-int dx[4] = { 0,1,0,-1 }; //오 아래 왼 위
+typedef vector<pi> vec2;
+typedef queue<pi> que;
+typedef vector<vector<pi>> gra;
+int dx[4] = { 0,1,0,-1 };
 int dy[4] = { 1,0,-1,0 };
-int res;
-int num;
-string str,str2;
 int N, M;
-vpi v;
-map<string, int>m;
-bool f;
-void solve(int idx,int idx2)
-{
-	if (f)return;
-	if (idx2 == str2.size() - 1)
-	{
-		if (str[idx] == '*' && (idx + 1) < str.size())
-			idx++;
-		if (str[idx] == str2[idx2]&&idx==str.size()-1)f = true;
-		return;
-	}
-	if (str[idx] == '*')
-	{
-		solve(idx, idx2 + 1);
-		solve(idx + 1, idx2);
-	}
-	else if (str[idx] == str2[idx2])
-		solve(idx + 1, idx2 + 1);
-	
-}
+int T;
+int K;
+int res;
+int num, num2;
+string A;
+string B;
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 	cout.tie(0);
+	int i, j;
 	cin >> N;
-	cin >> str;
-	for (int i = 0; i < N; ++i)
+	cin >> A;
+	string front;
+	string back;
+	int flag = 0;
+	for (i = 0; i < A.size(); i++)
 	{
-		cin >> str2;
-		solve(0,0);
-		if (f)cout << "DA";
+		if (A[i] == '*')
+		{
+			flag = 1;
+			continue;
+		}
+		if (!flag)
+			front.push_back(A[i]);
 		else
-			cout << "NE";
-		cout << "\n";
-		f = false;
-	}
-	
-	
+			back.push_back(A[i]);
 }
+	for (i = 0; i < N; i++)
+	{
+		cin >> B;
+		flag = 0;
+		if (front.size() + back.size() > B.size())
+			flag = 1;
+		else if (B.substr(0, front.size()) != front)
+			flag = 1;
+		else if (B.substr(B.size() - back.size(), back.size()) != back)
+			flag = 1;
+		if (!flag)
+			cout << "DA\n";
+		else
+			cout << "NE\n";
+	}
+
+}
+
+
+
+
+
