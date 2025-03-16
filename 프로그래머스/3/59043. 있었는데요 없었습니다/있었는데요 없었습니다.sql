692d8141e9d30b601e2ce0772e118eed874881b2
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2025-03-16 11:35:30 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -1,4 +1,4 @@
 -- 코드를 입력하세요
-SELECT a.ANIMAL_ID,a.NAME from ANIMAL_INS a join ANIMAL_OUTS b on a.ANIMAL_ID=b.ANIMAL_ID
-where a.datetime>b.datetime
+SELECT a.ANIMAL_ID,b.NAME from ANIMAL_INS a join ANIMAL_OUTS b on a.ANIMAL_ID=b.ANIMAL_ID
+where b.DATETIME<a.DATETIME
 order by a.datetime
