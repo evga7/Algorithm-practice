7f4143a6fa73fabfc4cc4e2939247400d5afb65e
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2025-04-08 23:37:43 +0900

    [Silver I] Title: 동물원, Time: 0 ms, Memory: 2284 KB -BaekjoonHub

--- 백준/Silver/1309. 동물원/동물원.cc
+++ 백준/Silver/1309. 동물원/동물원.cc
@@ -1,51 +1,17 @@
-
-#include <bits/stdc++.h>
-#define MAX 987654321
-#define MIN -987654321
-#define MOD 1000000000
-using namespace std;
-typedef long long int ll;
-typedef pair<int, int> pi;
-typedef pair<int, string> pi3;
-typedef pair<int, pair<int, int>>pi2;
-typedef vector<vector<int>> vpi;
-typedef vector<int> vec;
-typedef vector<pi> vec2;
-typedef queue<pi> que;
-typedef vector<vector<pi>> gra;
-int dx[4] = { 0,1,0,-1 };
-int dy[4] = { 1,0,-1,0 };
-int N, M;
-int res;
-int num, num2;
-int T;
-int K;
-int arr[501][501];
+#include <stdio.h>
 int dp[100001][3];
-
-int solve(int n,int m)
-{
-	if (n == N)return 1;
-	int& ret = dp[n][m];
-	if (ret != -1)return ret;
-	ret = 0;
-	ret = solve(n + 1, 0);
-	if (m != 1)
-		ret += solve(n + 1, 1)%9901;
-	if (m != 2)
-		ret += solve(n + 1, 2) % 9901;
-	return ret % 9901;
-}
 int main()
 {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
-	int i, j;
-	cin >> N;
-	memset(dp, -1, sizeof(dp));
-	cout << solve(0, 0) % 9901;
+	int N;
+	int i;
+	scanf("%d", &N);
+	dp[0][0] = { 0 };
+	dp[1][0] = dp[1][1] = dp[1][2] = 1;
+	for (i = 2; i <= N+1; i++)
+	{
+		dp[i][0] = (dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2]) % 9901;
+		dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % 9901;
+		dp[i][2] = (dp[i - 1][0] + dp[i - 1][1]) % 9901;
+	}
+	printf("%d", dp[N+1][0] % 9901);
 }
-
-
-
