57706c3a40b8dd740e05a7bfc7af7470a4eb0f15
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2025-04-11 23:40:54 +0900

    [Gold II] Title: 로봇 조종하기, Time: 188 ms, Memory: 111296 KB -BaekjoonHub

--- 백준/Gold/2169. 로봇 조종하기/로봇 조종하기.cc
+++ 백준/Gold/2169. 로봇 조종하기/로봇 조종하기.cc
@@ -1,63 +1,55 @@
-#include <bits/stdc++.h>
-#include<unordered_map>
-#define MAX 99999999999999999
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <string>
+#include <sstream>
+#include <stack>
+#include <set>
+#include <map>
+#define MAX 987654321
 #define MIN -987654321
-#define MOD 1000000007
-#define pb push_back
-#define pob pop_back
-#define FOR(i,a,b) for(int i = a; i < b; i++)
 using namespace std;
-typedef unsigned long long int ll;
+typedef long long int ll;
 typedef pair<int, int> pi;
-typedef pair<int, string> pi3;
-typedef pair<int, pair<int, int>>pi2;
-typedef vector<vector<int>> vvec;
-typedef vector<int> vec;
-typedef vector<pi> vec2;
-typedef queue<pi> que;
-typedef vector<vector<pi>> gra;
-int dx[4] = { 0,0,1,-1 };
-int dy[4] = { 1,-1,0,0 };
 int N, M;
-int L;
-int T;
-int res;
-int num, num2;
-int sum;
-string str;
-int dp[1001][1001][3];
-int a[1001][1001];
-int solve(int x, int y, int op) {
+int arr[1001][1001];
+ll dp[3][1001][1001];
+int visited[1001][1001];
+int dx[3] = { 0,0,1 };
+int dy[3] = { -1,1,0 };
+int solve(int dir,int x,int y)
+{
+	ll& ret = dp[dir][x][y];
 	if (x == N - 1 && y == M - 1)
-		return a[x][y];
-	int& ret = dp[x][y][op];
-	if (ret != -1)return ret;
-	ret = -987654321;
-	for (int i = 0; i < 3; i++)
-	{
-		if (op == 0 && i == 1)continue;
-		if (op == 1 && i == 0)continue;
-		int n_x = x + dx[i];
-		int n_y = y + dy[i];
-		if (0 <= n_x && n_x < N && 0 <= n_y && n_y < M) {
-			ret = max(ret, solve(n_x, n_y, i) + a[x][y]);
-		}
-	}
-	return ret;
-
+		return arr[x][y];
+	if (ret != MIN)
+		return ret;
+	int i;
+	ll n1, n2, n3;
+	n1 = n2 = n3 = MIN;
+	visited[x][y] = 1;
+	if (y - 1 >=0 &&! visited[x][y-1])
+		n1 = solve(0, x, y - 1);
+	if (y + 1 < M && !visited[x][y + 1])
+		n2 = solve(1, x, y + 1);
+	if (x + 1 < N && !visited[x + 1][y])
+		n3 = solve(2, x + 1, y);
+	visited[x][y] = 0;
+	return ret = arr[x][y] + max({ n1,n2,n3 });
 }
 int main()
 {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+	int i, j;
 	cin >> N >> M;
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++) {
-			cin >> a[i][j];
+	for (i = 0; i < N; i++)
+	{
+		for (j = 0; j < M; j++)
+		{
+			cin >> arr[i][j];
+			dp[0][i][j] = dp[1][i][j] = dp[2][i][j] = MIN;
 		}
 	}
-	memset(dp, -1, sizeof(dp));
-	cout << solve (0, 0, 2);
+	 cout<<solve(0,0,0);
 }
-
