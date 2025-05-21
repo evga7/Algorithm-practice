7d5ca09701f9dde0d06d8be94c7f8420fe17f244
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2025-05-21 18:30:20 +0900

    [level 1] Title: 특정 형질을 가지는 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
+++ 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
@@ -1,3 +1,3 @@
 -- 코드를 작성해주세요
-select count(*) COUNT from ECOLI_DATA 
-where (GENOTYPE&1=1 or GENOTYPE&4=4) and GENOTYPE&2!=2
+select count(*) COUNT from ECOLI_DATA
+where ((GENOTYPE & 1)=1 or (GENOTYPE & 4)=4) and (GENOTYPE &2=0)
