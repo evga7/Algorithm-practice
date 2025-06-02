98c702288a201ee929c3d08ff0e476253bc28a7c
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2025-06-02 17:32:54 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,4 +1,4 @@
 -- 코드를 입력하세요
-SELECT a.NAME,a.DATETIME from ANIMAL_INS a left outer join ANIMAL_OUTS b on a.ANIMAL_ID=b.ANIMAL_ID
-where a.NAME is not null and b.name is null
+SELECT a.NAME,a.DATETIME from animal_ins a left join animal_outs b on a.ANIMAL_ID=b.ANIMAL_ID
+where b.name is null and a.name is not null
 order by a.DATETIME limit 3
