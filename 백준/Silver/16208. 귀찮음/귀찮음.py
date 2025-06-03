04f084c98ae162ce96acb25a774a8c4ebe4fe0f4
Wonpyo Hong <44694917+evga7@users.noreply.github.com>
2025-06-03 23:48:24 +0900

    [Silver V] Title: 귀찮음, Time: 340 ms, Memory: 191016 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Silver/16208. 귀찮음/귀찮음.py
@@ -0,0 +1,30 @@
+# N,M=map(int,input().split())
+# visited=[[987654321]*M for _ in range(N)]
+# arr=[list(map(int,input().strip())) for _ in range(N)]
+# str=re.findall(r'\d+',str) 숫자만 때기
+# re.findall(r'-?\d+', str1)] 음수는 이렇게
+# word1 = re.findall("[a-zA-Z]+", st) 이건 문자 추출
+#arr2 = [k[::-1] for k in zip(*arr)] 문자열 90도 회전
+import bisect
+import math
+import re
+import heapq
+import sys
+from collections import *
+import itertools
+import collections
+si=sys.stdin.readline
+#sys.setrecursionlimit(10 ** 6)
+dx = [0,-1,0,1]  # 왼 위 오 아래
+dy = [-1,0,1,0]
+def input():return sys.stdin.readline().rstrip()
+N=int(input())
+arr=list(map(int,input().split()))
+arr.sort()
+res=0
+s=sum(arr)
+for cur in arr:
+    s-=cur
+    res+=cur*s
+
+print(res)
